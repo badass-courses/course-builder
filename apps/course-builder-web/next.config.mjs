eb91f2626dc2da468cfacec7acef15ec66219879
joel <joelhooks@gmail.com>
2023-11-13 10:33:17 -0800

    revert updates for build

--- apps/course-builder-web/next.config.mjs
+++ apps/course-builder-web/next.config.mjs
@@ -5,10 +5,6 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {
-  experimental: {
-    ppr: true,
-  }
-};
+const config = {};
 
 export default config;
