a747e436873340a16461e493ce644e240ab64154
joel <joelhooks@gmail.com>
2023-11-17 15:59:57 -0800

    try to make the env vars work 😭

--- apps/course-builder-web/src/env.mjs
+++ apps/course-builder-web/src/env.mjs
@@ -11,7 +11,7 @@ export const env = createEnv({
       .string()
       .url()
       .refine(
-        (str) => !str.includes('mysql://{USERNAME}:{PASSWORD}@aws.connect.psdb.cloud/{DATABASE_NAME}?ssl={"rejectUnauthorized":true}'),
+        (str) => !str.includes('YOUR_MYSQL_URL_HERE'),
         'You forgot to change the default URL',
       ),
     NODE_ENV: z
