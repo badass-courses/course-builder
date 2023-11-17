4630903bac7680f2998885ecd7ded0ea5362ff27
joel <joelhooks@gmail.com>
2023-11-17 14:48:21 -0800

    updates some build stuff

---
+++ packages/config/tsup/index.js
@@ -0,0 +1,14 @@
+const { execSync } = require("child_process");
+
+/** @type {import("tsup").Options} */
+const config = {
+  sourcemap: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  async onSuccess() {
+    // emit dts and sourcemaps to enable jump to definition
+    execSync("pnpm tsc --project tsconfig.sourcemap.json");
+  },
+};
+
+module.exports = { config };
