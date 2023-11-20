f3edfb0a20a80a044ec00105c0014bbdac8b70f0
joel <joelhooks@gmail.com>
2023-11-20 12:55:24 -0800

    walked through all the Starlight config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,23 +1,37 @@
-import starlight from '@astrojs/starlight'
-import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Course Builder',
-      social: {
-        github: 'https://github.com/joelhooks/course-builder',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Getting Started', link: '/guides/getting-started/' },
-          ],
-        },
-      ],
-    }),
-  ],
-})
+  site: 'https://docs.coursebuilder.dev/',
+  integrations: [starlight({
+    title: 'Course Builder',
+    logo: {
+      src: './src/assets/badass.svg',
+      replacesTitle: true,
+    },
+    editLink: {
+      baseUrl: 'https://github.com/joelhooks/course-builder/edit/main/docs/',
+    },
+    defaultLocale: 'root',
+    locales: {
+      root: {
+        label: 'English',
+        lang: 'en',
+      }
+    },
+    social: {
+      github: 'https://github.com/joelhooks/course-builder',
+      'x.com': 'https://x.com/badass_courses',
+    },
+    customCss: [
+      // Path to your Tailwind base styles:
+      './src/tailwind.css',
+    ],
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  })]
+});
