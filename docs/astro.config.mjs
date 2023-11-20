6f9877690055e72779feb7ecc6e7114588d93b21
joel <joelhooks@gmail.com>
2023-11-20 13:20:53 -0800

    ignore mdx in formatting checks

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,38 +1,40 @@
-import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.coursebuilder.dev/',
   favicon: '/images/badass.svg',
-  integrations: [starlight({
-    title: 'Course Builder',
-    logo: {
-      src: './src/assets/badass.svg',
-      replacesTitle: true,
-    },
-    editLink: {
-      baseUrl: 'https://github.com/joelhooks/course-builder/edit/main/docs/',
-    },
-    defaultLocale: 'root',
-    locales: {
-      root: {
-        label: 'English',
-        lang: 'en',
-      }
-    },
-    social: {
-      github: 'https://github.com/joelhooks/course-builder',
-      'x.com': 'https://x.com/badass_courses',
-    },
-    customCss: [
-      // Path to your Tailwind base styles:
-      './src/tailwind.css',
-    ],
-  }), tailwind({
-    // Disable the default base styles:
-    applyBaseStyles: false,
-  })]
-});
+  integrations: [
+    starlight({
+      title: 'Course Builder',
+      logo: {
+        src: './src/assets/badass.svg',
+        replacesTitle: true,
+      },
+      editLink: {
+        baseUrl: 'https://github.com/joelhooks/course-builder/edit/main/docs/',
+      },
+      defaultLocale: 'root',
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en',
+        },
+      },
+      social: {
+        github: 'https://github.com/joelhooks/course-builder',
+        'x.com': 'https://x.com/badass_courses',
+      },
+      customCss: [
+        // Path to your Tailwind base styles:
+        './src/tailwind.css',
+      ],
+    }),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
+})
