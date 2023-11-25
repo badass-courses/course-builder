24b5e21c23b2485f923ee6fa65a5ae0f82d05e64
joel <joelhooks@gmail.com>
2023-11-24 17:25:54 -0800

    bump next version

--- cli/template/base/next.config.js
+++ cli/template/base/next.config.js
@@ -7,9 +7,6 @@ await import('./src/env.js')
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
--- cli/template/extras/config/next-config-appdir.js
+++ cli/template/extras/config/next-config-appdir.js
@@ -5,10 +5,6 @@
 await import('./src/env.js')
 
 /** @type {import("next").NextConfig} */
-const config = {
-  experimental: {
-    serverActions: true,
-  },
-}
+const config = {}
 
 export default config
