eb0aa60f9f92e08542c51f336b7821b2be8e2c10
joel <joelhooks@gmail.com>
2023-12-04 09:20:11 -0800

    brings in pragmatic drag and drop

---
+++ apps/course-builder-web/src/components/lesson-list/list.tsx
@@ -0,0 +1,511 @@
+'use client'
+
+import React, {
+  createContext,
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+
+import ReactDOM from 'react-dom'
+import invariant from 'tiny-invariant'
+
+import {DropdownItem, DropdownItemGroup} from '@atlaskit/dropdown-menu'
+import {
+  draggable,
+  dropTargetForElements,
+  monitorForElements,
+} from '@atlaskit/pragmatic-drag-and-drop/adapter/element'
+import {combine} from '@atlaskit/pragmatic-drag-and-drop/util/combine'
+import {offsetFromPointer} from '@atlaskit/pragmatic-drag-and-drop/util/offset-from-pointer'
+import {reorder} from '@atlaskit/pragmatic-drag-and-drop/util/reorder'
+import {setCustomNativeDragPreview} from '@atlaskit/pragmatic-drag-and-drop/util/set-custom-native-drag-preview'
+import {triggerPostMoveFlash} from '@atlaskit/pragmatic-drag-and-drop-flourish/trigger-post-move-flash'
+import {
+  attachClosestEdge,
+  Edge,
+  extractClosestEdge,
+} from '@atlaskit/pragmatic-drag-and-drop-hitbox/addon/closest-edge'
+import {getReorderDestinationIndex} from '@atlaskit/pragmatic-drag-and-drop-hitbox/util/get-reorder-destination-index'
+import * as liveRegion from '@atlaskit/pragmatic-drag-and-drop-live-region'
+import {DragHandleDropdownMenu} from '@atlaskit/pragmatic-drag-and-drop-react-accessibility/drag-handle-dropdown-menu'
+import {DropIndicator} from '@atlaskit/pragmatic-drag-and-drop-react-indicator/box'
+import {Box, Grid, Stack, xcss} from '@atlaskit/primitives'
+import {token} from '@atlaskit/tokens'
+
+type ItemPosition = 'first' | 'last' | 'middle' | 'only'
+
+type CleanupFn = () => void
+
+type ListContextProps = {
+  getItemIndex: ({id}: {id: string}) => number
+  getItemPosition: (itemData: ItemData) => ItemPosition
+  registerItem: (args: {id: string; element: HTMLElement}) => CleanupFn
+  reorderItem: (args: {
+    startIndex: number
+    indexOfTarget: number
+    closestEdgeOfTarget: Edge | null
+  }) => void
+  instanceId: symbol
+}
+
+const ListContext = createContext<ListContextProps | null>(null)
+
+function useListContext() {
+  const listContext = useContext(ListContext)
+  invariant(listContext !== null)
+  return listContext
+}
+
+type ItemData = {
+  id: string
+  label: string
+}
+
+const listItemContainerStyles = xcss({
+  position: 'relative',
+  backgroundColor: 'elevation.surface',
+  borderWidth: 'border.width.0',
+  borderBottomWidth: token('border.width', '1px'),
+  borderStyle: 'solid',
+  borderColor: 'color.border',
+  ':last-of-type': {
+    borderWidth: 'border.width.0',
+  },
+})
+
+const listItemStyles = xcss({
+  position: 'relative',
+  padding: 'space.100',
+})
+
+const listItemDisabledStyles = xcss({opacity: 0.4})
+
+type DraggableState =
+  | {type: 'idle'}
+  | {type: 'preview'; container: HTMLElement}
+  | {type: 'dragging'}
+
+const idleState: DraggableState = {type: 'idle'}
+const draggingState: DraggableState = {type: 'dragging'}
+
+const listItemPreviewStyles = xcss({
+  paddingBlock: 'space.050',
+  paddingInline: 'space.100',
+  borderRadius: 'border.radius.100',
+  backgroundColor: 'elevation.surface.overlay',
+  maxWidth: '360px',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+})
+
+const itemLabelStyles = xcss({
+  flexGrow: 1,
+  whiteSpace: 'nowrap',
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+})
+
+function ListItem({itemData}: {itemData: ItemData}) {
+  const {getItemIndex, registerItem, instanceId} = useListContext()
+
+  const ref = useRef<HTMLDivElement>(null)
+  const [closestEdge, setClosestEdge] = useState<Edge | null>(null)
+
+  const triggerRef = useRef<HTMLButtonElement>(null)
+
+  const [draggableState, setDraggableState] =
+    useState<DraggableState>(idleState)
+
+  useEffect(() => {
+    invariant(ref.current)
+    invariant(triggerRef.current)
+
+    const element = ref.current
+
+    const dragData = {id: itemData.id, instanceId}
+
+    return combine(
+      registerItem({id: itemData.id, element}),
+      draggable({
+        element,
+        dragHandle: triggerRef.current,
+        getInitialData() {
+          return {...dragData, index: getItemIndex(itemData)}
+        },
+        onGenerateDragPreview({nativeSetDragImage}) {
+          setCustomNativeDragPreview({
+            nativeSetDragImage,
+            getOffset: offsetFromPointer({
+              x: token('space.200', '16px'),
+              y: token('space.100', '8px'),
+            }),
+            render({container}) {
+              setDraggableState({type: 'preview', container})
+
+              return () => setDraggableState(draggingState)
+            },
+          })
+        },
+        onDragStart() {
+          setDraggableState(draggingState)
+        },
+        onDrop() {
+          setDraggableState(idleState)
+        },
+      }),
+      dropTargetForElements({
+        element,
+        canDrop({source}) {
+          return source.data.instanceId === instanceId
+        },
+        getData({input}) {
+          return attachClosestEdge(dragData, {
+            element,
+            input,
+            allowedEdges: ['top', 'bottom'],
+          })
+        },
+        onDrag({self, source}) {
+          const isSource = source.element === element
+          if (isSource) {
+            setClosestEdge(null)
+            return
+          }
+
+          const closestEdge = extractClosestEdge(self.data)
+
+          const sourceIndex = source.data.index
+          invariant(typeof sourceIndex === 'number')
+
+          const selfIndex = getItemIndex({id: itemData.id})
+
+          const isItemBeforeSource = selfIndex === sourceIndex - 1
+          const isItemAfterSource = selfIndex === sourceIndex + 1
+
+          const isDropIndicatorHidden =
+            (isItemBeforeSource && closestEdge === 'bottom') ||
+            (isItemAfterSource && closestEdge === 'top')
+
+          if (isDropIndicatorHidden) {
+            setClosestEdge(null)
+            return
+          }
+
+          setClosestEdge(closestEdge)
+        },
+        onDragLeave() {
+          setClosestEdge(null)
+        },
+        onDrop() {
+          setClosestEdge(null)
+        },
+      }),
+    )
+  }, [getItemIndex, instanceId, itemData, registerItem])
+
+  return (
+    <Fragment>
+      <Box ref={ref} xcss={listItemContainerStyles}>
+        <Grid
+          alignItems="center"
+          columnGap="space.100"
+          templateColumns="auto 1fr auto"
+          xcss={[
+            listItemStyles,
+            /**
+             * We are applying the disabled effect to the inner element so that
+             * the border and drop indicator are not affected.
+             */
+            draggableState.type === 'dragging' && listItemDisabledStyles,
+          ]}
+        >
+          <DragHandleDropdownMenu
+            triggerRef={triggerRef}
+            label={`Reorder ${itemData.label}`}
+          >
+            <LazyDropdownContent itemData={itemData} />
+          </DragHandleDropdownMenu>
+          <Box xcss={itemLabelStyles}>{itemData.label}</Box>
+        </Grid>
+        {closestEdge && <DropIndicator edge={closestEdge} gap="1px" />}
+      </Box>
+      {draggableState.type === 'preview' &&
+        ReactDOM.createPortal(
+          <Box xcss={listItemPreviewStyles}>{itemData.label}</Box>,
+          draggableState.container,
+        )}
+    </Fragment>
+  )
+}
+
+function LazyDropdownContent({itemData}: {itemData: ItemData}) {
+  const {getItemIndex, getItemPosition, reorderItem} = useListContext()
+
+  const position = getItemPosition(itemData)
+
+  const isMoveUpDisabled = position === 'first' || position === 'only'
+  const isMoveDownDisabled = position === 'last' || position === 'only'
+
+  const moveUp = useCallback(() => {
+    const startIndex = getItemIndex(itemData)
+    reorderItem({
+      startIndex,
+      indexOfTarget: startIndex - 1,
+      closestEdgeOfTarget: null,
+    })
+  }, [getItemIndex, itemData, reorderItem])
+
+  const moveDown = useCallback(() => {
+    const startIndex = getItemIndex(itemData)
+    reorderItem({
+      startIndex,
+      indexOfTarget: startIndex + 1,
+      closestEdgeOfTarget: null,
+    })
+  }, [getItemIndex, itemData, reorderItem])
+
+  return (
+    <DropdownItemGroup>
+      <DropdownItem onClick={moveUp} isDisabled={isMoveUpDisabled}>
+        Move up
+      </DropdownItem>
+      <DropdownItem onClick={moveDown} isDisabled={isMoveDownDisabled}>
+        Move down
+      </DropdownItem>
+    </DropdownItemGroup>
+  )
+}
+
+const defaultItems: ItemData[] = [
+  {
+    id: 'task-1',
+    label: 'Eating a healthy breakfast',
+  },
+  {
+    id: 'task-2',
+    label: 'Create a new product roadmap',
+  },
+  {
+    id: 'task-3',
+    label: 'Update the company website',
+  },
+  {
+    id: 'task-4',
+    label: 'Plan the next team offsite',
+  },
+  {
+    id: 'task-5',
+    label: 'Coordinate with the design team',
+  },
+  {
+    id: 'task-6',
+    label: 'Manage the company blog',
+  },
+  {
+    id: 'task-7',
+    label: 'Organize the next team offsite',
+  },
+  {
+    id: 'task-8',
+    label: 'Coordinate with the design team',
+  },
+]
+
+const containerStyles = xcss({
+  maxWidth: '400px',
+  borderWidth: 'border.width',
+  borderStyle: 'solid',
+  borderColor: 'color.border',
+})
+
+type ListState = {
+  items: ItemData[]
+  lastCardMoved: {
+    item: ItemData
+    previousIndex: number
+    currentIndex: number
+    numberOfItems: number
+  } | null
+}
+
+export default function ListExample() {
+  const [{items, lastCardMoved}, setListState] = useState<ListState>({
+    items: defaultItems,
+    lastCardMoved: null,
+  })
+
+  const registryRef = useRef(new Map<string, HTMLElement>())
+  const registerItem = useCallback(
+    ({id, element}: {id: string; element: HTMLElement}) => {
+      const registry = registryRef.current
+      if (!registry) {
+        return () => {}
+      }
+      registry.set(id, element)
+
+      return function unregisterItem() {
+        registry.delete(id)
+      }
+    },
+    [],
+  )
+
+  useEffect(() => {
+    return () => {
+      liveRegion.cleanup()
+    }
+  }, [])
+
+  /**
+   * Creating a stable reference for the items, so that we can avoid
+   * rerenders.
+   */
+  const stableItemsRef = useRef<ItemData[]>(items)
+  useEffect(() => {
+    stableItemsRef.current = items
+  }, [items])
+
+  useEffect(() => {
+    if (lastCardMoved === null) {
+      return
+    }
+
+    const {item, previousIndex, currentIndex, numberOfItems} = lastCardMoved
+    const element = registryRef.current.get(item.id)
+    if (element) {
+      triggerPostMoveFlash(element)
+    }
+
+    liveRegion.announce(
+      `You've moved ${item.label} from position ${
+        previousIndex + 1
+      } to position ${currentIndex + 1} of ${numberOfItems}.`,
+    )
+  }, [lastCardMoved])
+
+  const reorderItem = useCallback(
+    ({
+      startIndex,
+      indexOfTarget,
+      closestEdgeOfTarget,
+    }: {
+      startIndex: number
+      indexOfTarget: number
+      closestEdgeOfTarget: Edge | null
+    }) => {
+      const finishIndex = getReorderDestinationIndex({
+        startIndex,
+        closestEdgeOfTarget,
+        indexOfTarget,
+        axis: 'vertical',
+      })
+
+      if (finishIndex === startIndex) {
+        // If there would be no change, we skip the update
+        return
+      }
+
+      setListState((listState) => {
+        const item = listState.items[startIndex] as ItemData
+
+        return {
+          items: reorder({
+            list: listState.items,
+            startIndex,
+            finishIndex,
+          }),
+          lastCardMoved: {
+            item,
+            previousIndex: startIndex,
+            currentIndex: finishIndex,
+            numberOfItems: listState.items.length,
+          },
+        }
+      })
+    },
+    [],
+  )
+
+  const [instanceId] = useState(() => Symbol('instance-id'))
+
+  useEffect(() => {
+    return monitorForElements({
+      canMonitor({source}) {
+        return source.data.instanceId === instanceId
+      },
+      onDrop({location, source}) {
+        const target = location.current.dropTargets[0]
+        if (!target) {
+          return
+        }
+
+        const items = stableItemsRef.current
+
+        const startIndex = items.findIndex((item) => item.id === source.data.id)
+        if (startIndex < 0) {
+          return
+        }
+
+        const indexOfTarget = items.findIndex(
+          (item) => item.id === target.data.id,
+        )
+        if (indexOfTarget < 0) {
+          return
+        }
+
+        const closestEdgeOfTarget = extractClosestEdge(target.data)
+
+        reorderItem({startIndex, indexOfTarget, closestEdgeOfTarget})
+      },
+    })
+  }, [instanceId, reorderItem])
+
+  const getItemPosition = useCallback((itemData: ItemData) => {
+    const items = stableItemsRef.current
+
+    if (items.length === 1) {
+      return 'only'
+    }
+
+    const index = items.indexOf(itemData)
+    if (index === 0) {
+      return 'first'
+    }
+
+    if (index === items.length - 1) {
+      return 'last'
+    }
+
+    return 'middle'
+  }, [])
+
+  const getItemIndex = useCallback(({id}: {id: string}) => {
+    return stableItemsRef.current.findIndex((item) => item.id === id)
+  }, [])
+
+  const contextValue = useMemo(() => {
+    return {
+      getItemIndex,
+      getItemPosition,
+      registerItem,
+      reorderItem,
+      instanceId,
+    }
+  }, [getItemIndex, getItemPosition, registerItem, reorderItem, instanceId])
+
+  return (
+    <ListContext.Provider value={contextValue}>
+      <Stack xcss={containerStyles}>
+        {items.map((itemData) => (
+          <ListItem key={itemData.id} itemData={itemData} />
+        ))}
+      </Stack>
+    </ListContext.Provider>
+  )
+}
--- docs/src/content/docs/decisions.md
+++ docs/src/content/docs/decisions.md
@@ -58,11 +58,56 @@ For now, Course Builder is going to stick with Next.js and TurboRepo as the solu
 
 One of the problems with this decision is that it makes it more difficult to use other systems that people might prefer. If somebody likes remix, for example, it's going to be difficult to make use of CourseBuilder as it is today. For that reason, this decision might actually change in the future and we might use something totally different or make it more generic or evolve it into something that is more representative of the wider field of options. But for now, we're sticking with Next.js and TurboRepo.
 
-## 03 Stripe
+## 03 Fork Create T3 App
 
-## 04 Sanity
+Date: 2023-12-01
+
+Status: accepted
+
+### Context
+
+we need specific tools for building our apps. consistency is key; we want each app to be similar. this requires a cli or generator and a template. using this setup, every new app will have the same base features.
+
+### Decision
+
+starting with create t3 app, well-built and decision-rich. our goal: tailor it for course-building apps. plan: fork create t3 app, integrate into our project. this base lets us develop a customized generator, leveraging t3 app's best practices.
+
+### Consequences
+
+forking t3 app, not just using it, means we handle updates ourselves. we can track t3's updates, but merging them won't be straightforward. we'll need to manually upgrade. this is manageable but important to remember.
+
+## 04 Inngest for Background Jobs
+Date: YYYY-MM-DD
+
+Status: proposed
+
+### Context
+In our customer-focused environment, we deal with various automated tasks and activities behind the scenes. Key interactions, like user sign-ups or lesson progressions, necessitate automated responses such as email notifications. This demands a robust system for processing background jobs.
+
+### Decision
+We have decided to use Inngest for managing our background jobs. 
+Inngest will handle the sending and processing of background tasks, offering 
+impressive local development features and the reliability and durability needed for cloud-hosted services in production.
+
+### Consequences
+Opting for Inngest ties us closely to its service. While we write in 
+JavaScript/TypeScript and maintain control over the application logic, 
+making migration feasible, Inngest integration adds another service to our system's architecture. This increases the complexity and dependency of our overall system structure.
 
 ## 05 PlanetScale
+
+Date: YYYY-MM-DD
+
+Status: proposed
+
+### Context
+For our individual courses and creators, we require dedicated databases to store crucial information like user data, product details, e-commerce transactions, and user progress. Each course or creator will have their own database setup.
+
+### Decision
+We have chosen to use PlanetScale for our database needs. PlanetScale stands out with its excellent hosted service features, including speed, reliability, and a serverless architecture. It aligns well with our requirements for a database service.
+
+### Consequences
+By selecting PlanetScale, we are committing to their service. Although exporting and transferring data is possible, it makes us dependent on another external service. This entails acquiring and managing credentials, along with ongoing management throughout our products' lifespans.
 
 ## 06 Vercel
 
