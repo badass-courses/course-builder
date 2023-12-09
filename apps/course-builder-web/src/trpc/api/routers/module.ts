f5fcdf08ea3f96ecbc1e8e8d8214a891ccec097a
joel <joelhooks@gmail.com>
2023-12-09 13:24:56 -0800

    feat: sorting the list updates in sanity

--- apps/course-builder-web/src/trpc/api/routers/module.ts
+++ apps/course-builder-web/src/trpc/api/routers/module.ts
@@ -1,8 +1,15 @@
 import {z} from 'zod'
 import {getServerAuthSession} from '@/server/auth'
 import {getAbility} from '@/lib/ability'
-import {createTRPCRouter, publicProcedure} from '@/trpc/api/trpc'
-import {sanityQuery} from '@/server/sanity.server'
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from '@/trpc/api/trpc'
+import {sanityMutation, sanityQuery} from '@/server/sanity.server'
+import {TRPCError} from '@trpc/server'
+import groq from 'groq'
+import {v4} from 'uuid'
 
 export const moduleRouter = createTRPCRouter({
   getBySlug: publicProcedure
@@ -16,7 +23,7 @@ export const moduleRouter = createTRPCRouter({
       const ability = getAbility({user: session?.user})
 
       if (!ability.can('upload', 'Media')) {
-        throw new Error('Unauthorized')
+        throw new TRPCError({code: 'UNAUTHORIZED'})
       }
 
       return input.slug
@@ -25,6 +32,57 @@ export const moduleRouter = createTRPCRouter({
         "videoResources": resources[@->._type == 'videoResource']->
       }`)
         : null
+    }),
+  updateTutorial: protectedProcedure
+    .input(
+      z.object({
+        tutorialId: z.string(),
+        updateData: z.object({
+          title: z.string(),
+          description: z.string().optional(),
+          lessons: z.array(z.object({_id: z.string()})),
+        }),
+      }),
+    )
+    .mutation(async ({ctx, input}) => {
+      const session = await getServerAuthSession()
+      const ability = getAbility({user: session?.user})
+
+      if (!ability.can('upload', 'Media')) {
+        throw new TRPCError({code: 'UNAUTHORIZED'})
+      }
+
+      const tutorial =
+        await sanityQuery(groq`*[_type == "module" && _id == "${input.tutorialId}"][0]{
+        _id,
+        resources[]->
+      }`)
+
+      if (!tutorial) {
+        throw new TRPCError({code: 'NOT_FOUND'})
+      }
+
+      return await sanityMutation(
+        [
+          {
+            patch: {
+              id: tutorial._id,
+              set: {
+                title: input.updateData.title,
+                description: input.updateData.description,
+                resources: input.updateData.lessons.map((lesson) => {
+                  return {
+                    _key: v4(),
+                    _type: 'reference',
+                    _ref: lesson._id,
+                  }
+                }),
+              },
+            },
+          },
+        ],
+        {returnDocuments: true},
+      )
     }),
   getTutorial: publicProcedure
     .input(
@@ -37,15 +95,22 @@ export const moduleRouter = createTRPCRouter({
       const ability = getAbility({user: session?.user})
 
       if (!ability.can('upload', 'Media')) {
-        throw new Error('Unauthorized')
+        throw new TRPCError({code: 'UNAUTHORIZED'})
       }
 
-      return await sanityQuery<{
+      return await getTutorial(input.slug)
+    }),
+})
+
+const getTutorial = async (slug?: string) => {
+  return slug
+    ? await sanityQuery<{
+        _id: string
         title: string
         description: string
         lessons: {_id: string; title: string}[]
       }>(
-        `*[_type == "module" && moduleType == 'tutorial' && (_id == "${input.slug}" || slug.current == "${input.slug}")][0]{
+        `*[_type == "module" && moduleType == 'tutorial' && (_id == "${slug}" || slug.current == "${slug}")][0]{
       ...,
       "lessons": resources[@->.moduleType == "lesson"]->{
        _id,
@@ -63,5 +128,5 @@ export const moduleRouter = createTRPCRouter({
       }
     }`,
       )
-    }),
-})
+    : null
+}
