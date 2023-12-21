60e96410e6c84b1f590f78d310c132b45d5a41e4
jbranchaud <jbranchaud@gmail.com>
2023-12-20 22:41:21 -0600

    feat: redirect unauthorized tip CRUD to /tips

--- apps/course-builder-web/src/app/tips/[slug]/edit/page.tsx
+++ apps/course-builder-web/src/app/tips/[slug]/edit/page.tsx
@@ -3,15 +3,20 @@ import {getServerAuthSession} from '@/server/auth'
 import {getAbility} from '@/lib/ability'
 import {getTip} from '@/lib/tips'
 import {EditTipForm} from '../../_components/edit-tip-form'
+import {redirect} from 'next/navigation'
 
 export default async function TipEditPage({params}: {params: {slug: string}}) {
   const session = await getServerAuthSession()
   const ability = getAbility({user: session?.user})
   const tip = await getTip(params.slug)
 
-  return tip && ability.can('upload', 'Media') ? (
+  if (!tip || !ability.can('upload', 'Media')) {
+    redirect('/tips')
+  }
+
+  return (
     <div className="relative mx-auto h-full w-full flex-grow items-center justify-center border-b">
       <EditTipForm key={tip.slug} tip={tip} />
     </div>
-  ) : null
+  )
 }
--- apps/course-builder-web/src/app/tips/new/page.tsx
+++ apps/course-builder-web/src/app/tips/new/page.tsx
@@ -2,14 +2,19 @@ import {getServerAuthSession} from '@/server/auth'
 import {getAbility} from '@/lib/ability'
 import {CreateTip} from '@/app/tips/_components/create-tip'
 import * as React from 'react'
+import {redirect} from 'next/navigation'
 
 export default async function NewTipPage() {
   const session = await getServerAuthSession()
   const ability = getAbility({user: session?.user})
 
+  if (!ability.can('upload', 'Media')) {
+    redirect('/tips')
+  }
+
   return (
     <div className="flex flex-col">
-      {ability.can('upload', 'Media') ? <CreateTip /> : null}
+      <CreateTip />
     </div>
   )
 }
