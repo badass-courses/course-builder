21e239eb510f05e4ea24a6679fe98eabcc5c55b2
joel <joelhooks@gmail.com>
2024-01-31 09:21:00 -0800

    fix some stuff

--- apps/course-builder-web/prettier.config.mjs
+++ apps/course-builder-web/prettier.config.mjs
@@ -2,31 +2,7 @@
 
 /** @type { PrettierConfig | import("@ianvs/prettier-plugin-sort-imports").PluginConfig} } */
 const config = {
-  arrowParens: 'always',
-  printWidth: 120,
-  singleQuote: true,
-  semi: false,
-  trailingComma: 'all',
-  useTabs: false,
-  tabWidth: 2,
-  plugins: [
-    '@ianvs/prettier-plugin-sort-imports',
-    'prettier-plugin-tailwindcss',
-  ],
-  // Last version that doesn't squash type and value imports
-  importOrderTypeScriptVersion: '4.4.0',
-  importOrder: [
-    '^(react/(.*)$)|^(react$)',
-    '^(next/(.*)$)|^(next$)',
-    '<THIRD_PARTY_MODULES>',
-    '',
-    '^@coursebuilder/(.*)$',
-    '^coursebuilder/(.*)$',
-    '',
-    '^~/(.*)$',
-    '^[./]',
-  ],
-  proseWrap: 'always', // printWidth line breaks in md/mdx
-}
+  plugins: ["prettier-plugin-tailwindcss"],
+};
 
-export default config
+export default config;
