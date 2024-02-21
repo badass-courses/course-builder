d03c4f5b3831a9160e004c694ca67c45b94d3837
Vojta Holik <vojta@egghead.io>
2024-02-21 12:40:22 +0100

    style: resizable panes and dark mode

--- apps/course-builder-web/src/components/navigation/theme-toggle.tsx
+++ apps/course-builder-web/src/components/navigation/theme-toggle.tsx
@@ -12,7 +12,7 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
+        <Button variant="ghost" size="icon" className="-mr-2">
           <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
