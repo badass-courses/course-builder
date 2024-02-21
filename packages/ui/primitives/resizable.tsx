badf5e5725b3e98baba86c4b8ec9c20b37edc90d
Vojta Holik <vojta@egghead.io>
2024-02-21 13:05:32 +0100

    style: resizable panes and dark mode

--- packages/ui/primitives/resizable.tsx
+++ packages/ui/primitives/resizable.tsx
@@ -15,15 +15,9 @@ const ResizablePanelGroup: React.FC<
   />
 )
 
-const ResizablePanel: React.FC<ResizablePrimitive.PanelProps> = ResizablePrimitive.Panel
+const ResizablePanel: React.FC<ResizablePrimitive.PanelProps> = (props) => <ResizablePrimitive.Panel {...props} />
 
-const ResizableHandle: React.FC<{ withHandle?: boolean; className?: string }> = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
-  withHandle?: boolean
-}) => (
+const ResizableHandle: React.FC<any & { withHandle?: boolean }> = ({ withHandle, className, ...props }) => (
   <ResizablePrimitive.PanelResizeHandle
     className={cn(
       'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
