5856f5468ffd2253bdf41aa56df540495c7d9e34
myk <mbilokonsky@gmail.com>
2024-03-02 18:37:18 -0500

    feat: wired up AWS S3+Textract for OCR code extraction

---
+++ apps/course-builder-web/src/utils/aws.ts
@@ -0,0 +1,46 @@
+import aws from 'aws-sdk'
+
+aws.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
+
+const s3 = new aws.S3()
+
+const textract = new aws.Textract()
+
+export async function performOCR(s3_bucket: string, s3_key: string) {
+  const params = {
+    Document: {
+      S3Object: {
+        Bucket: s3_bucket,
+        Name: s3_key,
+      },
+    },
+  }
+
+  const response = await textract.detectDocumentText(params).promise()
+
+  const text = response.Blocks?.map((block) => block.Text)
+
+  return text?.join('\n') ?? ''
+}
+
+export async function uploadImage(
+  s3_bucket: string,
+  s3_key: string,
+  image: Buffer,
+  contentType: string,
+  contentLength: number,
+) {
+  return await s3
+    .putObject({
+      Bucket: s3_bucket,
+      Key: s3_key,
+      ContentType: contentType,
+      ContentLength: contentLength,
+      Body: image,
+    })
+    .promise()
+}
