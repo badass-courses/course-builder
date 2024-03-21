6371a43397ee8b527937972cea549595e51b04f8
joel <joelhooks@gmail.com>
2024-03-21 08:39:47 -0700

    fix: no-op legacy inngest app

--- apps/course-builder-web/src/app/api/inngest/meh/route.ts
+++ apps/course-builder-web/src/app/api/inngest/meh/route.ts
@@ -5,11 +5,15 @@ export const maxDuration = 300
 
 const appName = `gpt-4-ai-chains`
 
+const inngestClient = new Inngest({
+  id: appName,
+})
+
+const noopFunction = inngestClient.createFunction({ id: 'noop' }, { event: 'noop' }, async () => {})
+
 const inngest = serve({
-  client: new Inngest({
-    id: appName,
-  }),
-  functions: [],
+  client: inngestClient,
+  functions: [noopFunction],
 })
 
 export const { GET, POST, PUT } = inngest
