9cdb4e5774d74bf96e76299d265e3d4ca89ab77c
joel <joelhooks@gmail.com>
2024-04-19 09:23:01 -0700

    fix: revert back to planetscale driver
    it was causing some type issues and should be a different commit/pr

--- apps/course-builder-web/src/db/index.ts
+++ apps/course-builder-web/src/db/index.ts
@@ -1,19 +1,17 @@
 import { mysqlTable } from '@/db/mysql-table'
 import { env } from '@/env.mjs'
-import { drizzle } from 'drizzle-orm/mysql2'
-import { createPool, Pool } from 'mysql2/promise'
+import { Client } from '@planetscale/database'
+import { drizzle } from 'drizzle-orm/planetscale-serverless'
 
 import { DrizzleAdapter } from '@coursebuilder/adapter-drizzle'
 
 import * as schema from './schema'
 
-const globalForDb = globalThis as unknown as {
-	conn: Pool | undefined
-}
-
-const conn = globalForDb.conn ?? createPool({ uri: env.DATABASE_URL })
-if (env.NODE_ENV !== 'production') globalForDb.conn = conn
-
-export const db = drizzle(conn, { schema, mode: 'default' })
+export const db = drizzle(
+	new Client({
+		url: env.DATABASE_URL,
+	}),
+	{ schema },
+)
 
 export const courseBuilderAdapter = DrizzleAdapter(db, mysqlTable)
