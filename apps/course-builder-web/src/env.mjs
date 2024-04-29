ce4a892ae18886c1eaa07e9b84dab8861223db1d
joel <joelhooks@gmail.com>
2024-04-29 08:49:22 -0700

    chore: try catch log

--- apps/course-builder-web/src/env.mjs
+++ apps/course-builder-web/src/env.mjs
@@ -9,14 +9,7 @@ export const env = createEnv({
 	server: {
 		STRIPE_SECRET_TOKEN: z.string(),
 		STRIPE_WEBHOOK_SECRET: z.string(),
-		COURSEBUILDER_URL: z.preprocess(
-			// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-			// Since NextAuth.js automatically uses the VERCEL_URL if present.
-			(str) =>
-				process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : str,
-			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
-			process.env.VERCEL ? z.string() : z.string(),
-		),
+		COURSEBUILDER_URL: z.string(),
 		DATABASE_URL: z
 			.string()
 			.url()
