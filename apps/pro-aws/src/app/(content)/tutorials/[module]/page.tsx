49af4364ea5f4340b585e28a9cebdc6f90e076ae
Vojta Holik <vojta@egghead.io>
2024-04-29 07:34:48 +0200

    style(proaws): tutorial route with image

--- apps/pro-aws/src/app/(content)/tutorials/[module]/page.tsx
+++ apps/pro-aws/src/app/(content)/tutorials/[module]/page.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import type { Metadata, ResolvingMetadata } from 'next'
 import Link from 'next/link'
 import { notFound, redirect } from 'next/navigation'
+import { CldImage } from '@/app/_components/cld-image'
+import { Contributor } from '@/app/_components/contributor'
 import { getTutorial } from '@/lib/tutorials-query'
 import { getServerAuthSession } from '@/server/auth'
 import { PencilIcon } from '@heroicons/react/24/outline'
@@ -42,26 +44,51 @@ export default async function ModulePage({ params }: Props) {
 		notFound()
 	}
 
+	const firstLesson = tutorial.resources[0]?.resource?.resources?.[0]?.resource
+
 	return (
-		<div className="container relative border-x px-0">
-			<div className="px-8">
-				<h1 className="font-heading py-16 text-5xl font-bold text-white sm:text-6xl lg:text-7xl">
-					{tutorial.fields.title}
-				</h1>
-				<p>{tutorial.fields.description}</p>
-				{ability.can('update', 'Content') && (
-					<Button
-						asChild
-						variant="secondary"
-						className="absolute right-5 top-5 gap-1"
-					>
-						<Link href={`/tutorials/${params.module}/edit`}>Edit</Link>
-					</Button>
+		<main className="container relative border-x px-0">
+			{ability.can('update', 'Content') && (
+				<Button
+					asChild
+					variant="secondary"
+					className="absolute right-5 top-5 gap-1"
+				>
+					<Link href={`/tutorials/${params.module}/edit`}>Edit</Link>
+				</Button>
+			)}
+			<div className="flex w-full flex-col-reverse items-center justify-between px-5 py-8 md:flex-row md:px-8 lg:px-16">
+				<div className="mt-5 flex w-full flex-col items-center text-center md:mt-0 md:items-start md:text-left">
+					<p className="text-primary mb-2 text-base">Free Tutorial</p>
+					<h1 className="font-heading text-balance text-5xl font-bold text-white sm:text-6xl lg:text-7xl">
+						{tutorial.fields.title}
+					</h1>
+					{tutorial.fields.description && (
+						<h2 className="mt-5 text-balance text-xl">
+							{tutorial.fields.description}
+						</h2>
+					)}
+					<Contributor className="mt-5" />
+					{firstLesson?.fields.slug && (
+						<Button asChild size="lg" className="mt-10 w-full md:w-auto">
+							<Link href={`${params.module}/${firstLesson?.fields.slug}`}>
+								Start Watching
+							</Link>
+						</Button>
+					)}
+				</div>
+				{tutorial.fields.coverImage?.url && (
+					<CldImage
+						width={400}
+						height={400}
+						src={tutorial.fields.coverImage.url}
+						alt={tutorial.fields.coverImage?.alt || ''}
+					/>
 				)}
 			</div>
 			<div className="flex flex-col border-t md:flex-row">
 				{tutorial.fields.body && (
-					<article className="prose sm:prose-lg prose-invert prose-headings:text-balance w-full max-w-none px-8 py-8">
+					<article className="prose sm:prose-lg prose-invert prose-headings:text-balance w-full max-w-none px-5 py-8 md:px-8">
 						<ReactMarkdown>{tutorial.fields.body}</ReactMarkdown>
 					</article>
 				)}
@@ -133,6 +160,6 @@ export default async function ModulePage({ params }: Props) {
 					})}
 				</div>
 			</div>
-		</div>
+		</main>
 	)
 }
