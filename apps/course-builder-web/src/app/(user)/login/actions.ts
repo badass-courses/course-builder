b19ea1c2b4b2eef936f9b8386abe7f6886c70fef
joel <joelhooks@gmail.com>
2024-04-29 08:47:35 -0700

    chore: try catch log

---
+++ apps/course-builder-web/src/app/(user)/login/actions.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { headers } from 'next/headers'
+import { env } from '@/env.mjs'
+
+export async function getCsrf() {
+	const headerStore = headers()
+	const cookie = headerStore.get('cookie')
+	const options: RequestInit = {
+		headers: {
+			...(cookie ? { cookie } : {}),
+		},
+		cache: 'no-cache',
+	}
+	console.log('csrf url', `${env.COURSEBUILDER_URL}/api/auth/csrf`)
+
+	return await fetch(`${env.COURSEBUILDER_URL}/api/auth/csrf`, options)
+		.then(async (res) => {
+			console.log('res', res)
+			const resonseText = await res.text()
+			console.log('resonseText', resonseText)
+			const { csrfToken } = JSON.parse(resonseText)
+			return csrfToken
+		})
+		.catch((e) => {
+			console.log(e)
+			throw e
+		})
+}
--- apps/course-builder-web/src/app/(user)/login/page.tsx
+++ apps/course-builder-web/src/app/(user)/login/page.tsx
@@ -1,53 +1,17 @@
 import { headers } from 'next/headers'
 import { Layout } from '@/components/app/layout'
 import { Login } from '@/components/login'
-import { env } from '@/env.mjs'
 import { getProviders } from '@/server/auth'
-import { getCsrfToken } from 'next-auth/react'
+
+import { getCsrf } from './actions'
 
 export const dynamic = 'force-dynamic'
 
 export default async function LoginPage() {
-	console.log('start login')
-	const headerStore = headers()
-	const cookie = headerStore.get('cookie')
-	const options: RequestInit = {
-		headers: {
-			...(cookie ? { cookie } : {}),
-		},
-		cache: 'no-cache',
-	}
-
-	console.log('options', options)
+	headers()
 
 	const providers = getProviders()
-
-	console.log('providers parsed', { providers })
-
-	console.log('csrf url', `${env.COURSEBUILDER_URL}/api/auth/csrf`)
-
-	const csrfToken = await fetch(
-		`${env.COURSEBUILDER_URL}/api/auth/csrf`,
-		options,
-	)
-		.then(async (res) => {
-			console.log('res', res)
-			try {
-				const resonseText = await res.text()
-				console.log('resonseText', resonseText)
-				const { csrfToken } = JSON.parse(resonseText)
-				return csrfToken
-			} catch (e) {
-				console.log('error', e)
-				throw e
-			}
-		})
-		.catch((e) => {
-			console.log(e)
-			throw e
-		})
-
-	console.log('csrfToken', csrfToken)
+	const csrfToken = await getCsrf()
 
 	return (
 		<Layout>
