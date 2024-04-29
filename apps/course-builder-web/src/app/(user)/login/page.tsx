f5386e94990b95d00c736ab724162f7dd9772f71
joel <joelhooks@gmail.com>
2024-04-29 08:04:45 -0700

    chore: add logging to login

--- apps/course-builder-web/src/app/(user)/login/page.tsx
+++ apps/course-builder-web/src/app/(user)/login/page.tsx
@@ -3,27 +3,35 @@ import { Layout } from '@/components/app/layout'
 import { Login } from '@/components/login'
 import { env } from '@/env.mjs'
 import { getProviders } from '@/server/auth'
-import { getCsrfToken } from 'next-auth/react'
 
 export const dynamic = 'force-dynamic'
 
 export default async function LoginPage() {
+	console.log('start login')
 	const headerStore = headers()
 	const cookie = headerStore.get('cookie')
-	console.log({ cookie })
 	const options: RequestInit = {
 		headers: {
 			...(cookie ? { cookie } : {}),
 		},
 	}
+
 	const providers = getProviders()
+
+	console.log('providers parsed', { providers })
 
 	const { csrfToken } = await fetch(
 		`${env.COURSEBUILDER_URL}/api/auth/csrf`,
 		options,
-	).then((res) => res.json())
+	)
+		.then((res) => res.json())
+		.catch((e) => {
+			console.log(e)
+			throw e
+		})
 
-	console.log({ providers, csrfToken })
+	console.log('csrfToken', csrfToken)
+
 	return (
 		<Layout>
 			<Login csrfToken={csrfToken} providers={providers} />
