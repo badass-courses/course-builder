b258487e889e5e7039dc61ddeae535bf15fb58b2
joel <joelhooks@gmail.com>
2024-05-02 08:17:14 -0700

    feat: dynamic mysql loader with docker for tests

--- packages/adapter-drizzle/test/mysql/test.sh
+++ packages/adapter-drizzle/test/mysql/test.sh
@@ -9,6 +9,7 @@ if [[ "$MODE" == "run" || "$MODE" == "watch" ]]; then
   MYSQL_ROOT_PASSWORD=password
   MYSQL_CONTAINER_NAME=coursebuilder-mysql-test
 
+  echo "Starting MySQL container..."
   docker run -d --rm \
     -e MYSQL_DATABASE=${MYSQL_DATABASE} \
     -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
@@ -16,22 +17,35 @@ if [[ "$MODE" == "run" || "$MODE" == "watch" ]]; then
     -p 3306:3306 \
     mysql:8.0.37
 
-  echo "Waiting 10s for db to start..." && sleep 10
+  echo "Waiting for the database to be ready..."
+  while ! docker exec ${MYSQL_CONTAINER_NAME} mysqladmin ping -h localhost --silent; do
+    sleep 1
+  done
+  sleep 1
+  echo "Database is ready."
 
-  # Push schema and seed
+  echo "Generating MySQL schema..."
   NODE_OPTIONS='--import tsx' drizzle-kit generate:mysql --config=./test/mysql/drizzle.config.ts
+
+  echo "Pushing MySQL schema..."
   NODE_OPTIONS='--import tsx' drizzle-kit push:mysql --config=./test/mysql/drizzle.config.ts
 
   if [[ "$MODE" == "run" ]]; then
+    echo "Running tests..."
     if vitest run -c ../utils/vitest.config.ts ./test/mysql/index.test.ts; then
+      echo "Tests passed. Stopping MySQL container..."
       docker stop ${MYSQL_CONTAINER_NAME}
     else
+      echo "Tests failed. Stopping MySQL container..."
       docker stop ${MYSQL_CONTAINER_NAME} && exit 1
     fi
   elif [[ "$MODE" == "watch" ]]; then
+    echo "Running tests in watch mode..."
     if vitest watch -c ../utils/vitest.config.ts ./test/mysql/index.test.ts; then
+      echo "Watch mode exited. Stopping MySQL container..."
       docker stop ${MYSQL_CONTAINER_NAME}
     else
+      echo "Watch mode exited with an error. Stopping MySQL container..."
       docker stop ${MYSQL_CONTAINER_NAME} && exit 1
     fi
   fi
