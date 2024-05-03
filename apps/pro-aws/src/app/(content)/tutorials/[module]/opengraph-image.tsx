4fb59d3e759498a203f546d45086e3ddcf84af07
Vojta Holik <vojta@egghead.io>
2024-05-03 13:31:12 +0200

    style(proaws): tutorial og image

--- apps/pro-aws/src/app/(content)/tutorials/[module]/opengraph-image.tsx
+++ apps/pro-aws/src/app/(content)/tutorials/[module]/opengraph-image.tsx
@@ -28,33 +28,67 @@ export default async function TutorialOG({
 	const rift = fetch(
 		new URL('../../../../styles/fonts/rift_600_normal.woff', import.meta.url),
 	).then((res) => res.arrayBuffer())
+	const dmSans = fetch(
+		new URL('../../../../../public/fonts/DMSans-Medium.ttf', import.meta.url),
+	).then((res) => res.arrayBuffer())
 
 	return new ImageResponse(
 		(
 			<div
-				tw="flex h-full w-full bg-black flex-col"
+				tw="flex h-full w-full bg-black flex-col py-10 px-24"
 				style={{
-					...font('rift'),
-					backgroundImage: `url("${process.env.NEXT_PUBLIC_URL}/assets/og-bg@2x.jpg")`,
+					backgroundImage: `url("${process.env.NEXT_PUBLIC_URL}/assets/og-bg-simple@2x.jpg")`,
 					backgroundSize: '1200px 630px',
 					width: 1200,
 					height: 630,
+					padding: 10,
+					borderBottom: '8px solid #F28D5A',
 				}}
 			>
-				<main tw="flex py-10 pr-10 bg-background flex-row w-full gap-5 h-full flex-grow items-center text-center justify-center">
-					{resource?.fields?.coverImage?.url && (
-						<img
-							src={resource?.fields?.coverImage?.url}
-							width={400}
-							height={400}
-						/>
-					)}
-					<div tw="flex flex-col">
-						<div tw="text-[40px] text-white text-sans text-[#F28F5A]">
+				<main tw="flex bg-background flex-row w-full gap-5 h-full flex-grow items-center text-center justify-center">
+					<div tw="flex flex-col pl-40">
+						<div
+							tw="text-[32px] text-white text-[#F28F5A]"
+							style={{
+								...font('dmsans'),
+							}}
+						>
 							Free Tutorial
 						</div>
-						<div tw="text-[90px] text-white">{resource?.fields?.title}</div>
+						<div
+							tw="text-[86px] text-white"
+							style={{
+								...font('rift'),
+							}}
+						>
+							{resource?.fields?.title}
+						</div>
+						<div tw="flex items-center mt-10">
+							<img
+								src={`${process.env.NEXT_PUBLIC_URL}/instructor.png`}
+								width={80}
+								height={80}
+								tw="rounded-md"
+							/>
+							<span
+								tw="text-3xl text-white font-sans ml-5"
+								style={{
+									...font('dmsans'),
+								}}
+							>
+								Adam Elmore
+							</span>
+						</div>
 					</div>
+					{resource?.fields?.coverImage?.url && (
+						<div tw="flex mr-10">
+							<img
+								src={resource?.fields?.coverImage?.url}
+								width={480}
+								height={480}
+							/>
+						</div>
+					)}
 				</main>
 			</div>
 		),
@@ -65,6 +99,10 @@ export default async function TutorialOG({
 				{
 					name: 'rift',
 					data: await rift,
+				},
+				{
+					name: 'dmsans',
+					data: await dmSans,
 				},
 			],
 		},
