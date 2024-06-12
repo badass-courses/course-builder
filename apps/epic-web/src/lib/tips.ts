8b70b92cf2d6e36369aa570203a3e14f142fb8c8
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-06-12 18:07:29 +0200

    fix TipSchema

--- apps/epic-web/src/lib/tips.ts
+++ apps/epic-web/src/lib/tips.ts
@@ -18,22 +18,30 @@ export const TipVisibilitySchema = z.union([
 
 export const TipSchema = ContentResourceSchema.merge(
 	z.object({
-		contributions: z.array(
-			z.object({
-				user: z.object({
-					id: z.string(),
-					name: z.string().optional().nullable(),
-					email: z.string().optional().nullable(),
-					image: z.string().optional().nullable(),
+		contributions: z
+			.array(
+				z.object({
+					user: z
+						.object({
+							id: z.string(),
+							name: z.string().optional().nullable(),
+							email: z.string().optional().nullable(),
+							image: z.string().optional().nullable(),
+						})
+						.optional()
+						.nullable(),
+					contributionType: z
+						.object({
+							id: z.string(),
+							slug: z.string(),
+							name: z.string(),
+							description: z.string().optional(),
+						})
+						.optional()
+						.nullable(),
 				}),
-				contributionType: z.object({
-					id: z.string(),
-					slug: z.string(),
-					name: z.string(),
-					description: z.string().optional(),
-				}),
-			}),
-		),
+			)
+			.optional(),
 		fields: z.object({
 			title: z.string(),
 			summary: z.string().optional().nullable(),
