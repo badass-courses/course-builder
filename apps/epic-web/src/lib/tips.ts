50e323abb1ab93b99c7122e668c4c5ce97032b4e
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-06-21 21:57:01 +0200

    pull muxPlaybackId to the Tip schema and query

--- apps/epic-web/src/lib/tips.ts
+++ apps/epic-web/src/lib/tips.ts
@@ -42,6 +42,17 @@ export const TipSchema = ContentResourceSchema.merge(
 			visibility: TipVisibilitySchema.default('unlisted'),
 			slug: z.string(),
 		}),
+		resources: z.array(
+			z.object({
+				resourceId: z.string(),
+				resource: z.object({
+					id: z.string(),
+					fields: z.object({
+						muxPlaybackId: z.string(),
+					}),
+				}),
+			}),
+		),
 	}),
 )
 
