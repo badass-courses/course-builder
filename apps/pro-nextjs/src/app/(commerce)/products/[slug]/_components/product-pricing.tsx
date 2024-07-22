ae3ca64b1a04dc6d7d8395da40d82fa0714343af
Vojta Holik <vojta@egghead.io>
2024-07-22 15:31:38 +0200

    update(pnjs): cleaned up product pricing

--- apps/pro-nextjs/src/app/(commerce)/products/[slug]/_components/product-pricing.tsx
+++ apps/pro-nextjs/src/app/(commerce)/products/[slug]/_components/product-pricing.tsx
@@ -3,14 +3,12 @@
 import * as React from 'react'
 import { usePathname } from 'next/navigation'
 import { env } from '@/env.mjs'
-import { pricingClassNames } from '@/styles/commerce'
 
 import { PriceCheckProvider } from '@coursebuilder/commerce-next/pricing/pricing-check-context'
-import {
-	PricingData,
-	PricingWidget,
-} from '@coursebuilder/commerce-next/pricing/pricing-widget'
+import { PricingData } from '@coursebuilder/commerce-next/pricing/pricing-widget'
 import { CommerceProps } from '@coursebuilder/commerce-next/utils/commerce-props'
+
+import { PricingWidget } from './pricing-widget'
 
 export function ProductPricing({
 	product,
@@ -27,10 +25,6 @@ export function ProductPricing({
 	purchasedProductIds: string[]
 	hasPurchasedCurrentProduct?: boolean
 }) {
-	const dataAttr = product.type === 'live' ? 'data-event' : 'data-default'
-	const Wrapper = (props: React.PropsWithChildren<{ className?: string }>) =>
-		React.createElement('div', { [dataAttr]: '', ...props })
-
 	const teamQuantityLimit =
 		product.type === 'live'
 			? quantityAvailable && quantityAvailable > 5
@@ -38,37 +32,25 @@ export function ProductPricing({
 				: quantityAvailable
 			: 100
 
-	const pathname = usePathname()
-	const cancelUrl = process.env.NEXT_PUBLIC_URL + pathname
+	const cancelUrl = `${env.NEXT_PUBLIC_URL}/products/${product.fields?.slug || product.id}`
 
-	return (
-		<>
-			{product && (
-				<Wrapper className={pricingClassNames('flex flex-col items-center')}>
-					{product.type === 'live' && (
-						<h1 className="font-heading mx-auto inline-flex w-full max-w-2xl items-center justify-center text-balance px-5 pb-10 text-center text-5xl font-bold leading-tight">
-							{product?.name}
-						</h1>
-					)}
-					<PriceCheckProvider purchasedProductIds={purchasedProductIds}>
-						<PricingWidget
-							commerceProps={{ ...commerceProps, products: [product] }}
-							hasPurchasedCurrentProduct={hasPurchasedCurrentProduct}
-							product={product}
-							quantityAvailable={quantityAvailable}
-							pricingDataLoader={pricingDataLoader}
-							pricingWidgetOptions={{
-								withImage: product.type !== 'live',
-								withGuaranteeBadge: product.type !== 'live',
-								isLiveEvent: product.type === 'live',
-								teamQuantityLimit,
-								isPPPEnabled: product.type !== 'live',
-								cancelUrl: `${env.NEXT_PUBLIC_URL}/products/${product.fields?.slug || product.id}`,
-							}}
-						/>
-					</PriceCheckProvider>
-				</Wrapper>
-			)}
-		</>
-	)
+	return product ? (
+		<PriceCheckProvider purchasedProductIds={purchasedProductIds}>
+			<PricingWidget
+				commerceProps={{ ...commerceProps, products: [product] }}
+				hasPurchasedCurrentProduct={hasPurchasedCurrentProduct}
+				product={product}
+				quantityAvailable={quantityAvailable}
+				pricingDataLoader={pricingDataLoader}
+				pricingWidgetOptions={{
+					withImage: product.type !== 'live',
+					withGuaranteeBadge: product.type !== 'live',
+					isLiveEvent: product.type === 'live',
+					teamQuantityLimit,
+					isPPPEnabled: product.type !== 'live',
+					cancelUrl: cancelUrl,
+				}}
+			/>
+		</PriceCheckProvider>
+	) : null
 }
