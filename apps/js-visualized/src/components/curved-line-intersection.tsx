f2593513330feb62c112d96eab88cc195a933bc6
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-07-31 16:50:07 +0200

    refactpr CurvedLineIntersection so that it can be gradiented

--- apps/js-visualized/src/components/curved-line-intersection.tsx
+++ apps/js-visualized/src/components/curved-line-intersection.tsx
@@ -2,28 +2,44 @@ import React from 'react'
 import { cn } from '@/utils/cn'
 
 const commonStyles =
-	'bg-background absolute h-[71px] w-[71px] before:absolute before:inset-0 before:border-border before:content-[""]'
+	'bg-background absolute h-[71px] w-[71px] before:absolute before:inset-0 before:content-[""]'
 
 const CurvedLineIntersection: React.FC<
-	React.PropsWithChildren<{ className?: string }>
-> = ({ className }) => {
+	React.PropsWithChildren<{
+		className?: string
+		startColor?: string
+		endColor?: string
+	}>
+> = ({
+	className,
+	startColor = 'hsl(var(--border))',
+	endColor = 'hsl(var(--border))',
+}) => {
 	return (
-		<div className="relative h-[141px] w-full">
+		<div
+			className="relative h-[141px] w-full"
+			style={
+				{
+					'--start-color': startColor,
+					'--end-color': endColor,
+				} as React.CSSProperties
+			}
+		>
 			<div
 				className={cn(
-					'absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_bottom,transparent_calc(50%_-_0.5px),hsla(var(--border))_calc(50%_-_0.5px),hsla(var(--border))_calc(50%_+_0.5px),transparent_calc(50%_+_0.5px))]',
+					'absolute inset-0 before:absolute before:inset-x-0 before:top-[70px] before:h-px before:bg-gradient-to-r before:from-[var(--start-color)] before:to-[var(--end-color)] before:content-[""]',
 					className,
 				)}
 			>
 				<div
 					className={cn(
-						'bottom-0 left-0 before:rounded-tl-full before:border-l before:border-t',
+						'bottom-0 left-0 before:rounded-tl-full before:border-l before:border-t before:border-[var(--start-color)]',
 						commonStyles,
 					)}
 				/>
 				<div
 					className={cn(
-						'right-0 top-0 before:rounded-br-full before:border-b before:border-r ',
+						'right-0 top-0 before:rounded-br-full before:border-b before:border-r before:border-[var(--end-color)]',
 						commonStyles,
 					)}
 				/>
