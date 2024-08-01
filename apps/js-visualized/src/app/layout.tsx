5835efe31a291bff821e39ebd87f3e071f624e1f
Yevhen Nahalskyi <evgeniy.nagalskiy@gmail.com>
2024-08-01 18:55:11 +0200

    og image and favicons and metadata

--- apps/js-visualized/src/app/layout.tsx
+++ apps/js-visualized/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import '@/styles/globals.css'
 
 import * as React from 'react'
+import { Metadata } from 'next'
 import { Party } from '@/app/_components/party'
 import { Providers } from '@/app/_components/providers'
 import Navigation from '@/components/navigation'
 import { ThemeProvider } from '@/components/theme-provider'
+import config from '@/config'
+import { env } from '@/env.mjs'
 import { TRPCReactProvider } from '@/trpc/react'
 import { ourFileRouter } from '@/uploadthing/core'
 import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin'
@@ -12,10 +15,24 @@ import { GeistSans } from 'geist/font/sans'
 import { AxiomWebVitals } from 'next-axiom'
 import { extractRouterConfig } from 'uploadthing/server'
 
-export const metadata = {
-	title: 'JS Visualized',
-	description: 'Powered by Course Builder',
-	icons: [{ rel: 'icon', url: '/favicon.ico' }],
+export const metadata: Metadata = {
+	metadataBase: new URL(env.NEXT_PUBLIC_URL),
+	title: `${config.defaultTitle} by ${config.author}`,
+	description: config.description,
+	icons: [
+		{ rel: 'icon', sizes: 'any', url: '/favicon.ico' },
+		{ rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+	],
+	twitter: {
+		card: 'summary_large_image',
+	},
+	openGraph: {
+		images: [
+			{
+				url: config.openGraph.images[0]!.url,
+			},
+		],
+	},
 }
 
 export default function RootLayout({
