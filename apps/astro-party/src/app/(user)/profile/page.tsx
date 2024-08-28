0a80d6b7a5b715469a7c942268f3d1298f55f168
joel <joelhooks@gmail.com>
2024-08-28 15:59:00 -0700

    fix: layout for pages

--- apps/astro-party/src/app/(user)/profile/page.tsx
+++ apps/astro-party/src/app/(user)/profile/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from 'next/navigation'
+import { Layout } from '@/components/layout'
 import { getServerAuthSession } from '@/server/auth'
 
 import EditProfileForm from './_components/edit-profile-form'
@@ -8,16 +9,18 @@ export default async function ProfilePage() {
 
 	if (ability.can('read', 'User', session?.user?.id)) {
 		return (
-			<div className="mx-auto flex w-full max-w-screen-lg flex-col items-start gap-8 px-5 py-20 sm:gap-10 sm:py-16 md:flex-row lg:gap-16">
-				<header className="w-full md:max-w-[230px]">
-					<h1 className="fluid-xl text-center font-bold md:text-left">
-						Your Profile
-					</h1>
-				</header>
-				<main className="flex w-full flex-col space-y-10 md:max-w-md">
-					<EditProfileForm user={session.user} />
-				</main>
-			</div>
+			<Layout>
+				<div className="mx-auto flex w-full max-w-screen-lg flex-col items-start gap-8 px-5 py-20 sm:gap-10 sm:py-16 md:flex-row lg:gap-16">
+					<header className="w-full md:max-w-[230px]">
+						<h1 className="fluid-xl text-center font-bold md:text-left">
+							Your Profile
+						</h1>
+					</header>
+					<main className="flex w-full flex-col space-y-10 md:max-w-md">
+						<EditProfileForm user={session.user} />
+					</main>
+				</div>
+			</Layout>
 		)
 	}
 
