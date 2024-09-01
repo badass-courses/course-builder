541c498dc86303364d307c0c2509a46df0fc4c02
joel <joelhooks@gmail.com>
2024-09-01 10:26:39 -0700

    fix: send in a mock stripe provider if one isn't supplied

--- packages/adapter-drizzle/src/index.ts
+++ packages/adapter-drizzle/src/index.ts
@@ -1,8 +1,12 @@
 import { is } from 'drizzle-orm'
-import { MySqlDatabase, type MySqlTableFn } from 'drizzle-orm/mysql-core'
+import {
+	MySqlDatabase,
+	mysqlTable,
+	type MySqlTableFn,
+} from 'drizzle-orm/mysql-core'
 
 import { type CourseBuilderAdapter } from '@coursebuilder/core/adapters'
-import { Provider } from '@coursebuilder/core/providers'
+import { MockStripeProvider } from '@coursebuilder/core/providers/stripe'
 import { PaymentsProviderConfig } from '@coursebuilder/core/types'
 
 import {
@@ -14,14 +18,18 @@ import { type SqlFlavorOptions, type TableFn } from './lib/utils.js'
 export function DrizzleAdapter<SqlFlavor extends SqlFlavorOptions>(
 	db: SqlFlavor,
 	table: TableFn<SqlFlavor>,
-	paymentProvider: PaymentsProviderConfig,
+	paymentProvider: PaymentsProviderConfig = MockStripeProvider,
 ): CourseBuilderAdapter {
 	if (is(db, MySqlDatabase)) {
-		return mySqlDrizzleAdapter(db, table as MySqlTableFn, paymentProvider)
+		return mySqlDrizzleAdapter(
+			db,
+			(table as MySqlTableFn) || mysqlTable,
+			paymentProvider,
+		)
 	}
 
 	throw new Error(
-		`Unsupported database type (${typeof db}) in Auth.js Drizzle adapter.`,
+		`Unsupported database type (${typeof db}) in Course Builder Drizzle adapter.`,
 	)
 }
 
