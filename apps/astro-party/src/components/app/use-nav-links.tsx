32ff829233b31ce2f98f2cde481ab37c3ab37fa3
Vojta Holik <vojta@egghead.io>
2024-09-02 10:34:32 +0200

    chore(ap): events links in nav

--- apps/astro-party/src/components/app/use-nav-links.tsx
+++ apps/astro-party/src/components/app/use-nav-links.tsx
@@ -4,12 +4,26 @@ import { api } from '@/trpc/react'
 import type { NavLinkItem } from '../navigation/nav-link-item'
 
 export function useNavLinks() {
-	// const { data: availableEvents, status } = api.events.get.useQuery()
+	const { data: availableEvents, status } = api.events.get.useQuery()
 
 	return [
 		{
 			href: '/posts',
 			label: 'Posts',
+		},
+		{
+			href: '/events',
+			label: (
+				<>
+					Events
+					{availableEvents && availableEvents.length > 0 && (
+						<div
+							className="bg-brand-red mb-1 ml-1 h-1 w-1 rounded-full"
+							aria-label={`${availableEvents.length} scheduled`}
+						/>
+					)}
+				</>
+			),
 		},
 		// {
 		// 	href: '/articles',
@@ -18,20 +32,6 @@ export function useNavLinks() {
 		// {
 		// 	href: '/tips',
 		// 	label: 'Tips',
-		// },
-		// {
-		// 	href: '/events',
-		// 	label: (
-		// 		<>
-		// 			Events
-		// 			{availableEvents && availableEvents.length > 0 && (
-		// 				<div
-		// 					className="mb-1 ml-1 h-1 w-1 rounded-full bg-rose-400"
-		// 					aria-label={`${availableEvents.length} scheduled`}
-		// 				/>
-		// 			)}
-		// 		</>
-		// 	),
 		// },
 	] as NavLinkItem[]
 }
