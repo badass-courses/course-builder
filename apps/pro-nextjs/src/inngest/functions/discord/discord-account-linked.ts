50bb9a02cfadf809d728c67861e01600f381d6d8
joel <joelhooks@gmail.com>
2024-09-02 13:13:20 -0700

    try to add role better

--- apps/pro-nextjs/src/inngest/functions/discord/discord-account-linked.ts
+++ apps/pro-nextjs/src/inngest/functions/discord/discord-account-linked.ts
@@ -1,11 +1,11 @@
 import { db } from '@/db'
-import { users } from '@/db/schema'
+import { accounts, users } from '@/db/schema'
 import { env } from '@/env.mjs'
 import { OAUTH_PROVIDER_ACCOUNT_LINKED_EVENT } from '@/inngest/events/oauth-provider-account-linked'
 import { inngest } from '@/inngest/inngest.server'
 import { DiscordError, DiscordMember } from '@/lib/discord'
 import { fetchAsDiscordBot, fetchJsonAsDiscordBot } from '@/lib/discord-query'
-import { eq } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
 
 export const discordAccountLinked = inngest.createFunction(
 	{
@@ -29,6 +29,8 @@ export const discordAccountLinked = inngest.createFunction(
 			})
 		})
 
+		if (!user) throw new Error('No user found')
+
 		const discordUser = await step.run('get discord user', async () => {
 			const userUrl = new URL('https://discord.com/api/users/@me')
 			const userRes = await fetch(userUrl.toString(), {
@@ -50,46 +52,75 @@ export const discordAccountLinked = inngest.createFunction(
 			)
 		})
 
-		await step.sleep('give discord a moment', 300)
+		await step.sleep('give discord a moment', '10s')
 
-		const discordMember = await step.run('get discord member', async () => {
-			return await fetchJsonAsDiscordBot<DiscordMember | DiscordError>(
-				`guilds/${env.DISCORD_GUILD_ID}/members/${discordUser.id}`,
-			)
-		})
+		const discordAccount = await step.run(
+			'check if discord is connected',
+			async () => {
+				return db.query.accounts.findFirst({
+					where: and(
+						eq(accounts.userId, user.id),
+						eq(accounts.provider, 'discord'),
+					),
+				})
+			},
+		)
 
-		await step.run('update basic discord roles for user', async () => {
-			if ('user' in discordMember) {
-				const userHasPurchases =
-					(user?.purchases?.filter(
+		if (discordAccount) {
+			let discordMember = await step.run('get discord member', async () => {
+				return await fetchJsonAsDiscordBot<DiscordMember | DiscordError>(
+					`guilds/${env.DISCORD_GUILD_ID}/members/${discordAccount.providerAccountId}`,
+				)
+			})
+
+			await step.run('update basic discord roles for user', async () => {
+				if ('user' in discordMember) {
+					const validPurchases = user.purchases.filter(
 						(purchase) =>
 							purchase.status === 'Valid' || purchase.status === 'Restricted',
-					).length || 0) > 0
-				return await fetchAsDiscordBot(
-					`guilds/${env.DISCORD_GUILD_ID}/members/${discordMember.user.id}`,
-					{
-						method: 'PATCH',
-						body: JSON.stringify({
-							roles: Array.from(
-								new Set([
-									...discordMember.roles,
-									...(userHasPurchases ? [env.DISCORD_MEMBER_ROLE_ID] : []),
-								]),
-							),
-						}),
-						headers: {
-							'Content-Type': 'application/json',
-						},
-					},
-				)
-			}
-			return null
-		})
+					)
 
-		return {
-			account,
-			profile,
-			user: event.user,
+					const userHasPurchases = validPurchases.length > 0
+
+					const roles = Array.from(
+						new Set([
+							...discordMember.roles,
+							...(userHasPurchases ? [env.DISCORD_MEMBER_ROLE_ID] : []),
+						]),
+					)
+
+					console.info('roles', { roles })
+
+					return await fetchAsDiscordBot(
+						`guilds/${env.DISCORD_GUILD_ID}/members/${discordAccount.providerAccountId}`,
+						{
+							method: 'PATCH',
+							body: JSON.stringify({
+								roles,
+							}),
+							headers: {
+								'Content-Type': 'application/json',
+							},
+						},
+					)
+				}
+				return null
+			})
+
+			discordMember = await step.run('reload discord member', async () => {
+				return await fetchJsonAsDiscordBot<DiscordMember | DiscordError>(
+					`guilds/${env.DISCORD_GUILD_ID}/members/${discordAccount.providerAccountId}`,
+				)
+			})
+
+			return {
+				account,
+				profile,
+				user: event.user,
+				discordMember,
+			}
 		}
+
+		return 'no discord account found for user'
 	},
 )
