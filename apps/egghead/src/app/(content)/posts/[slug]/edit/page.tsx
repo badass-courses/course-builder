4f842d0ee6c1777fd9c8df8c472952b287185c33
Zac Jones <zacjones93@gmail.com>
2024-09-17 16:05:49 -0600

    feat: redirect to post if cannot manage

--- apps/egghead/src/app/(content)/posts/[slug]/edit/page.tsx
+++ apps/egghead/src/app/(content)/posts/[slug]/edit/page.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import { Layout } from '@/components/app/layout'
 import { courseBuilderAdapter } from '@/db'
 import { getPost } from '@/lib/posts-query'
 import { getServerAuthSession } from '@/server/auth'
+import { subject } from '@casl/ability'
 
 import { EditPostForm } from '../../_components/edit-post-form'
 
@@ -19,6 +20,10 @@ export default async function PostPage({
 
 	if (!post || !ability.can('create', 'Content')) {
 		notFound()
+	}
+
+	if (ability.cannot('manage', subject('Content', post))) {
+		redirect(`/${post?.fields?.slug}`)
 	}
 
 	const resource = post.resources?.[0]?.resource.id
