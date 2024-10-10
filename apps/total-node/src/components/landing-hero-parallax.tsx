9778a6083a433e285ee4e0e0aad75caba58a309b
Vojta Holik <vojta@egghead.io>
2024-10-10 16:41:02 +0200

    style(tn): landing tweaks

--- apps/total-node/src/components/landing-hero-parallax.tsx
+++ apps/total-node/src/components/landing-hero-parallax.tsx
@@ -16,6 +16,53 @@ function useParallax(value: MotionValue<number>, distance: number) {
 	return useTransform(value, [0, 1], [-distance, distance])
 }
 
+function useStylesForImage(name: string, scrollYProgress: MotionValue) {
+	const platformY = useSpring(useTransform(scrollYProgress, [0, 1], [1, 100]), {
+		damping: 20,
+		stiffness: 100,
+	})
+	const platformScale = useSpring(
+		useTransform(scrollYProgress, [0, 1], [1, 1.5]),
+		{ damping: 20, stiffness: 100 },
+	)
+	const mazeY = useSpring(useTransform(scrollYProgress, [0, 1], [0, 50]), {
+		damping: 20,
+		stiffness: 100,
+	})
+	const cloudsY = useSpring(useTransform(scrollYProgress, [0, 1], [0, 300]), {
+		damping: 20,
+		stiffness: 100,
+	})
+	const cloudsScale = useSpring(
+		useTransform(scrollYProgress, [0, 1], [1, 1.1]),
+		{ damping: 20, stiffness: 100 },
+	)
+
+	switch (name) {
+		case 'platform':
+			return {
+				y: platformY,
+				scale: platformScale,
+				transformOrigin: 'bottom',
+			}
+		case 'maze':
+			return {
+				y: mazeY,
+			}
+		case 'beam':
+			return {
+				y: mazeY,
+			}
+		case 'clouds':
+			return {
+				y: mazeY,
+				scale: cloudsScale,
+			}
+		default:
+			return {}
+	}
+}
+
 export const LandingHeroParallax = () => {
 	const scrollAreaRef = React.useRef(null)
 	const { scrollY, scrollYProgress } = useScroll({
@@ -23,68 +70,39 @@ export const LandingHeroParallax = () => {
 		offset: ['0%', '100%'],
 	})
 
-	const platformY = useTransform(scrollYProgress, [0, 1], [1, 100])
-	const platformScale = useTransform(scrollYProgress, [0, 1], [1, 1.5])
-	const mazeY = useTransform(scrollYProgress, [0, 1], [0, 50])
-	const cloudsY = useTransform(scrollYProgress, [0, 1], [0, 300])
-	const cloudsScale = useTransform(scrollYProgress, [0, 1], [1, 1.1])
-	const asSpringValue = (value: MotionValue) =>
-		useSpring(value, { damping: 20, stiffness: 100 })
+	const images = [
+		{ name: 'bg', src: '/assets/hero-bg.jpg' },
+		{ name: 'maze', src: '/assets/hero-maze.png' },
+		{ name: 'beam', src: '/assets/hero-beam.png' },
+		{ name: 'clouds', src: '/assets/hero-clouds.png' },
+		{ name: 'platform', src: '/assets/hero-platform.png' },
+	]
+
+	const animatedStyles = images.map(({ name }) =>
+		useStylesForImage(name, scrollYProgress),
+	)
 
 	return (
 		<div
 			ref={scrollAreaRef}
 			className="absolute bottom-0 left-0 aspect-square h-auto w-full overflow-hidden lg:aspect-[1920/1080]"
 		>
-			{[
-				{ name: 'bg', src: '../../public/assets/hero-bg.jpg' },
-				{ name: 'maze', src: '../../public/assets/hero-maze.png' },
-				{ name: 'beam', src: '../../public/assets/hero-beam.png' },
-				{ name: 'clouds', src: '../../public/assets/hero-clouds.png' },
-				{ name: 'platform', src: '../../public/assets/hero-platform.png' },
-			].map(({ name, src }) => {
-				const getStylesForImageName = (name: string) => {
-					switch (name) {
-						case 'platform':
-							return {
-								y: asSpringValue(platformY),
-								scale: asSpringValue(platformScale),
-								transformOrigin: 'bottom',
-							}
-						case 'maze':
-							return {
-								y: asSpringValue(mazeY),
-							}
-						case 'beam':
-							return {
-								y: asSpringValue(mazeY),
-							}
-						case 'clouds':
-							return {
-								y: asSpringValue(mazeY),
-								scale: asSpringValue(cloudsScale),
-							}
-						default:
-							return {}
-					}
-				}
-
-				return (
-					<motion.div
-						key={name}
-						style={getStylesForImageName(name)}
-						transition={{}}
-						className="absolute left-0 top-0 h-full w-full"
-					>
-						<Image
-							src={require(src)}
-							className={cn('object-cover object-bottom')}
-							alt=""
-							fill
-						/>
-					</motion.div>
-				)
-			})}
+			{images.map(({ name, src }, index) => (
+				<motion.div
+					key={name}
+					style={animatedStyles[index]}
+					transition={{}}
+					className="absolute left-0 top-0 h-full w-full"
+				>
+					<Image
+						src={src}
+						className={cn('object-cover object-bottom')}
+						alt=""
+						fill
+						priority
+					/>
+				</motion.div>
+			))}
 		</div>
 	)
 }
