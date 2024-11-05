9d66888a5934cf6e4e498c9d5a647e9d049ed028
joel <joelhooks@gmail.com>
2024-11-04 20:51:55 -0800

    chore: fix api routes

--- apps/value-based-design/src/app/api/auth/[...nextauth]/route.ts
+++ apps/value-based-design/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,1 @@
-export {
-	/* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
-	GET /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/,
-	POST,
-} from '@/server/auth'
+export { GET, POST } from '@/server/auth'
--- apps/value-based-design/src/app/api/coursebuilder/[...nextCourseBuilder]/route.ts
+++ apps/value-based-design/src/app/api/coursebuilder/[...nextCourseBuilder]/route.ts
@@ -1,5 +1,1 @@
-export {
-	/* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/
-	POST /* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/,
-	GET,
-} from '@/coursebuilder/course-builder-config'
+export { POST, GET } from '@/coursebuilder/course-builder-config'
