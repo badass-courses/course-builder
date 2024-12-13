b0a2f9d2129460cb22e5ee88776b68ab8332d490
Vojta Holik <vojta@egghead.io>
2024-12-13 15:41:20 +0100

    feat(egghead): spike search

--- apps/egghead/src/app/(search)/q/_components/instantsearch/hit.tsx
+++ apps/egghead/src/app/(search)/q/_components/instantsearch/hit.tsx
@@ -8,7 +8,8 @@ export default function Hit({ hit }: { hit: TypesenseResource }) {
 		<li className="">
 			<Link
 				className="group flex flex-col gap-2 py-3 sm:flex-row sm:items-center sm:justify-between sm:py-1.5 "
-				href={`/${hit.slug}`}
+				target={'_blank'}
+				href={`https://egghead.io${hit.path}`}
 			>
 				<div className="flex items-center gap-2">
 					{hit.image && <img src={hit.image} alt={hit.title} className="w-5" />}
