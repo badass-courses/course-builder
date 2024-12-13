f96ddff51e28e2aa97e724a2350f7eadd55914c0
Vojta Holik <vojta@egghead.io>
2024-12-13 14:26:04 +0100

    feat(egghead): spike search

--- apps/egghead/src/app/(search)/q/page.tsx
+++ apps/egghead/src/app/(search)/q/page.tsx
@@ -1,5 +1,7 @@
 import Search from './_components/search'
 
+export const dynamic = 'force-dynamic'
+
 export default async function SearchPage() {
 	return (
 		<main className="container max-w-4xl py-10">
