d4ea6291a05f4b7373871f3aaf86fb847506f3a4
Zac Jones <zacjones93@gmail.com>
2024-12-17 10:55:37 -0700

    feat(egh): implement createCourseInEgghead function for course creation
    - Added the `createCourseInEgghead` function to handle course creation in Egghead.
    - Integrated error handling for instructor retrieval and course creation.
    - Emitted `EGGHEAD_COURSE_CREATED_EVENT` upon successful course creation.
    - Updated the database to sync the created course ID with the corresponding post.
    
    This enhancement improves the workflow for creating courses and ensures proper event handling and data synchronization.
    
    Co-authored-by: Creeland <cree@provinsal.com>

--- apps/egghead/src/inngest/functions/egghead/create-course-in-egghead.ts
+++ apps/egghead/src/inngest/functions/egghead/create-course-in-egghead.ts
@@ -1,7 +1,11 @@
+import { db } from '@/db'
+import { contentResource } from '@/db/schema'
+import { EGGHEAD_COURSE_CREATED_EVENT } from '@/inngest/events/egghead/course-created'
 import { POST_CREATED_EVENT } from '@/inngest/events/post-created'
 import { inngest } from '@/inngest/inngest.server'
 import { createEggheadCourse } from '@/lib/egghead'
 import { loadEggheadInstructorForUser } from '@/lib/users'
+import { eq } from 'drizzle-orm'
 import { NonRetriableError } from 'inngest'
 
 export const createCourseInEgghead = inngest.createFunction(
@@ -36,8 +40,48 @@ export const createCourseInEgghead = inngest.createFunction(
 			return instructor
 		})
 
+		if (!instructor) {
+			throw new NonRetriableError('Instructor not found', {
+				cause: {
+					postId: post.id,
+					instructorId: post.createdById,
+				},
+			})
+		}
+
 		const course = await step.run('create-course', async () => {
-			const { fields } = post
+			const courseGuid = post.fields?.slug.split('~').pop()
+
+			const course = await createEggheadCourse({
+				title: post.fields?.title,
+				guid: courseGuid,
+				ownerId: instructor.user_id,
+			})
+
+			return course
+		})
+
+		step.sendEvent(EGGHEAD_COURSE_CREATED_EVENT, {
+			name: EGGHEAD_COURSE_CREATED_EVENT,
+			data: {
+				id: course.id,
+			},
+		})
+
+		await step.run('sync-course-id-to-builder', async () => {
+			await db
+				.update(contentResource)
+				.set({
+					fields: {
+						...post.fields,
+						eggheadCourseId: course.id,
+					},
+				})
+				.where(eq(contentResource.id, post.id))
+				.catch((error) => {
+					console.error('🚨 Error creating post', error)
+					throw error
+				})
 		})
 	},
 )
