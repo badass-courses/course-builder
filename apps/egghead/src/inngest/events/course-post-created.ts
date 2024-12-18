0ee8a18bb1c7f7bb8646ad833ad3c3f5a1c96726
Zac Jones <zacjones93@gmail.com>
2024-12-18 12:08:48 -0700

    fix(egh): use PostSchema for COURSE_POST_CREATED_EVENT

--- apps/egghead/src/inngest/events/course-post-created.ts
+++ apps/egghead/src/inngest/events/course-post-created.ts
@@ -1,3 +1,4 @@
+import { PostSchema } from '@/lib/posts'
 import { z } from 'zod'
 
 import { ContentResourceSchema } from '@coursebuilder/core/schemas'
@@ -11,9 +12,9 @@ export type CoursePostCreated = {
 	}
 }
 
-export const CoursePostCreatedEventSchema = ContentResourceSchema.merge(
+export const CoursePostCreatedEventSchema = PostSchema.merge(
 	z.object({
-		fields: ContentResourceSchema.shape.fields.and(
+		fields: PostSchema.shape.fields.and(
 			z.object({
 				postType: z.literal('course'),
 			}),
