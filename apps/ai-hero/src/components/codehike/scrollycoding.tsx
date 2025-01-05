23ade7838bf5fa1e65545f43890d1e92a52033bd
Vojta Holik <vojta@egghead.io>
2025-01-05 16:43:21 +0100

    feat(ai): add basic scrollycoding components from codehike

--- apps/ai-hero/src/components/codehike/scrollycoding.tsx
+++ apps/ai-hero/src/components/codehike/scrollycoding.tsx
@@ -1,4 +1,5 @@
 // https://github.com/code-hike/examples/blob/next-mdx-remote/with-next-mdx-remote-rsc/app/scrollycoding/page.tsx
+import React from 'react'
 import { tokenTransitions } from '@/components/codehike/code'
 import { cn } from '@/utils/cn'
 import { Block, CodeBlock, parseRoot } from 'codehike/blocks'
@@ -8,13 +9,13 @@ import {
 	Selection,
 	SelectionProvider,
 } from 'codehike/utils/selection'
+import { ArrowDown, ChevronDown } from 'lucide-react'
 import type { MDXContent } from 'mdx/types'
 import { z } from 'zod'
 
 const Schema = Block.extend({
 	steps: z.array(Block.extend({ code: CodeBlock })),
 })
-
 export default function Scrollycoding(props: any) {
 	console.log('Scrollycoding props', props)
 	if (!props) return null
@@ -22,40 +23,59 @@ export default function Scrollycoding(props: any) {
 	// const { steps } = parseRoot(props, Schema)
 	const { steps } = props
 
-	return (
-		<SelectionProvider className="relative flex-col gap-4 xl:flex xl:flex-row">
-			<div className="xl:mb-[90vh] xl:flex-1">
-				{steps.map((step: any, i: number) => (
-					<Selectable
-						key={i}
-						index={i}
-						selectOn={['click', 'scroll']}
-						// className="data-[selected=true]:border-primary mb-24 rounded border-l-4 px-5 py-2"
-						className="data-[selected=true]:border-primary -mx-5 mb-10 border-l-4 px-5"
-					>
-						<h2 className="mt-4 text-xl">{step.title}</h2>
-						<div>{step.children}</div>
-					</Selectable>
-				))}
-			</div>
+	const getHeadingLevel = (header: string) => {
+		const match = header?.match(/^(#{1,6})\s/)
+		return match ? match[1]?.length : 2 // default to h2 if no match
+	}
 
-			<div
-				className={cn(
-					'sticky bottom-0 -mx-5  lg:mx-auto lg:w-[40vw] lg:max-w-xl',
-					{
-						// padding top dynamic based on height of the first selectable item
-					},
-				)}
-			>
-				<div className="sticky top-10 overflow-auto lg:sticky lg:top-16">
-					<Selection
-						from={steps.map((step: any) => (
-							<Code codeblock={step.code} />
-						))}
-					/>
+	const renderHeading = (header: string) => {
+		const level = getHeadingLevel(header)
+		const H = `h${level}` as any
+		// Remove both the markdown heading symbols and the !!steps prefix
+		const cleanContent = header.replace(/^#{1,6}\s/, '').replace('!!steps ', '')
+
+		if (cleanContent === '!!steps') return null
+		return <H className="text-xl">{cleanContent}</H>
+	}
+
+	return (
+		<div className="mt-8">
+			<p className="not-prose flex w-full items-center justify-end gap-1 py-3 text-center font-mono text-xs font-medium uppercase tracking-wide text-gray-400">
+				Scrollycoding
+				<ArrowDown size={12} />
+			</p>
+			<SelectionProvider className="relative mb-10 flex-col gap-4 sm:mb-24 xl:flex xl:flex-row">
+				<div className="mb-10 flex flex-col gap-10 xl:flex-1">
+					{steps.map((step: any, i: number) => (
+						<Selectable
+							key={i}
+							index={i}
+							selectOn={['click', 'scroll']}
+							className="data-[selected=true]:border-primary first-of-type:prose-headings:mt-5 -mx-5 border-l-4 px-5"
+						>
+							{step._data?.header && renderHeading(step._data.header)}
+							<div>{step.children}</div>
+						</Selectable>
+					))}
 				</div>
-			</div>
-		</SelectionProvider>
+				<div
+					className={cn(
+						'sticky bottom-0 -mx-6 lg:mx-auto lg:w-[40vw] lg:max-w-xl',
+						{
+							// padding top dynamic based on height of the first selectable item
+						},
+					)}
+				>
+					<div className="sticky top-10 overflow-auto lg:sticky lg:top-16">
+						<Selection
+							from={steps.map((step: any) => (
+								<Code codeblock={step.code} />
+							))}
+						/>
+					</div>
+				</div>
+			</SelectionProvider>
+		</div>
 	)
 }
 
@@ -73,7 +93,7 @@ async function Code({
 		<Pre
 			code={highlighted}
 			handlers={[tokenTransitions]}
-			className="bg-background max-h-[200px] text-xs sm:max-h-full sm:text-sm"
+			className="mb-0 mt-0 h-full max-h-[300px] !rounded-none border-t bg-[#0d1117] p-5 text-xs shadow-inner sm:max-h-full sm:text-sm"
 		/>
 	)
 }
