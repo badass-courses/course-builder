2be7e914195d8e06085ec0546269696678098d86
Vojta Holik <vojta@egghead.io>
2025-01-06 07:25:18 +0100

    feat(ai): add basic scrollycoding components from codehike

--- packages/ui/codemirror/editor.tsx
+++ packages/ui/codemirror/editor.tsx
@@ -203,28 +203,54 @@ export const CourseBuilderEditorThemeLight = createTheme({
 	styles: [
 		{ tag: t.heading1, class: 'cm-heading-1' },
 		{ tag: t.link, color: '#0080ff' },
+		{
+			tag: [t.monospace, t.regexp, t.meta],
+		},
 		// github light
-		{ tag: [t.standard(t.tagName), t.tagName], color: '#116329' },
-		{ tag: [t.comment, t.bracket], color: '#6a737d' },
-		{ tag: [t.className, t.propertyName], color: '#6f42c1' },
+		{
+			tag: [t.standard(t.tagName), t.tagName],
+			color: '#116329',
+			fontFamily: 'monospace',
+		},
+		{
+			tag: [t.comment, t.bracket],
+			color: '#6a737d',
+			fontFamily: 'monospace',
+		},
+		{
+			tag: [t.className, t.propertyName],
+			color: '#6f42c1',
+			fontFamily: 'monospace',
+		},
 		{
 			tag: [t.variableName, t.attributeName, t.number, t.operator],
 			color: '#005cc5',
+			fontFamily: 'monospace',
 		},
 		{
 			tag: [t.keyword, t.typeName, t.typeOperator, t.typeName],
 			color: '#d73a49',
+			fontFamily: 'monospace',
 		},
-		{ tag: [t.string, t.meta, t.regexp], color: '#032f62' },
-		{ tag: [t.name, t.quote], color: '#22863a' },
+		{
+			tag: [t.string, t.meta, t.regexp],
+			color: '#032f62',
+			fontFamily: 'monospace',
+		},
+		{ tag: [t.name, t.quote], color: '#22863a', fontFamily: 'monospace' },
 		{ tag: [t.heading, t.strong], color: '#24292e', fontWeight: 'bold' },
 		{ tag: [t.emphasis], color: '#24292e', fontStyle: 'italic' },
-		{ tag: [t.deleted], color: '#b31d28', backgroundColor: 'ffeef0' },
+		{
+			tag: [t.deleted],
+			color: '#b31d28',
+			backgroundColor: 'ffeef0',
+			fontFamily: 'monospace',
+		},
 		{ tag: [t.atom, t.bool, t.special(t.variableName)], color: '#e36209' },
 		{ tag: [t.url, t.escape, t.regexp, t.link], color: '#0080ff' },
 		{ tag: t.link, textDecoration: 'underline' },
 		{ tag: t.strikethrough, textDecoration: 'line-through' },
-		{ tag: t.invalid, color: '#cb2431' },
+		{ tag: t.invalid, color: '#cb2431', fontFamily: 'monospace' },
 	],
 })
 export const CourseBuilderEditorThemeDark = createTheme({
@@ -243,29 +269,77 @@ export const CourseBuilderEditorThemeDark = createTheme({
 		gutterForeground: 'hsla(0, 0%, 100%, 0.3)',
 	},
 	styles: [
-		{ tag: t.heading1, class: 'cm-heading-1' },
-		{ tag: t.link, color: '#0080ff' },
+		{
+			tag: t.heading1,
+			class: 'cm-heading-1',
+		},
+		{
+			tag: t.link,
+			color: '#0080ff',
+		},
 		// github dark
-		{ tag: [t.standard(t.tagName), t.tagName], color: '#7ee787' },
-		{ tag: [t.comment, t.bracket], color: '#8b949e' },
-		{ tag: [t.className, t.propertyName], color: '#d2a8ff' },
+		{
+			tag: [t.standard(t.tagName), t.tagName],
+			color: '#7ee787',
+			fontFamily: 'monospace',
+		},
+		{
+			tag: [t.comment, t.bracket],
+			color: '#8b949e',
+			fontFamily: 'monospace',
+		},
+		{
+			tag: [t.className, t.propertyName],
+			color: '#d2a8ff',
+			fontFamily: 'monospace',
+		},
 		{
 			tag: [t.variableName, t.attributeName, t.number, t.operator],
 			color: '#79c0ff',
+			fontFamily: 'monospace',
 		},
 		{
 			tag: [t.keyword, t.typeName, t.typeOperator, t.typeName],
 			color: '#ff7b72',
+			fontFamily: 'monospace',
 		},
-		{ tag: [t.string, t.meta, t.regexp], color: '#a5d6ff' },
-		{ tag: [t.name, t.quote], color: '#7ee787' },
-		{ tag: [t.heading, t.strong], color: '#d2a8ff', fontWeight: 'bold' },
-		{ tag: [t.emphasis], color: '#d2a8ff', fontStyle: 'italic' },
-		{ tag: [t.deleted], color: '#ffdcd7', backgroundColor: 'ffeef0' },
+		{
+			tag: [t.string, t.meta, t.regexp],
+			color: '#a5d6ff',
+			fontFamily: 'monospace',
+		},
+		{
+			tag: [t.name, t.quote],
+			color: '#7ee787',
+			fontFamily: 'monospace',
+		},
+		{
+			tag: [t.heading, t.strong],
+			color: '#d2a8ff',
+			fontWeight: 'bold',
+		},
+		{
+			tag: [t.emphasis],
+			color: '#d2a8ff',
+			fontStyle: 'italic',
+		},
+		{
+			tag: [t.deleted],
+			color: '#ffdcd7',
+			backgroundColor: 'ffeef0',
+			fontFamily: 'monospace',
+		},
 		{ tag: [t.atom, t.bool, t.special(t.variableName)], color: '#ffab70' },
-		{ tag: [t.url, t.escape, t.regexp, t.link], color: '#79c0ff' },
+		{
+			tag: [t.url, t.escape, t.regexp, t.link],
+			color: '#79c0ff',
+		},
 		{ tag: t.link, textDecoration: 'underline' },
 		{ tag: t.strikethrough, textDecoration: 'line-through' },
-		{ tag: t.invalid, color: '#f97583' },
+		{
+			tag: t.invalid,
+			color: '#f97583',
+			fontFamily: 'monospace',
+		},
 	],
 })
