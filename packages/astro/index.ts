17d3c919b592f49eca4f1ab8a688ec5b0625181c
joel <joelhooks@gmail.com>
2025-02-06 11:14:46 -0800

    export the CourseBuilder config from astro package

--- packages/astro/index.ts
+++ packages/astro/index.ts
@@ -3,3 +3,5 @@ import integration from './src/integration'
 export type Integration = typeof integration
 export default integration
 export { defineConfig } from './src/config'
+
+export type { CoursebuilderConfig, FullCoursebuilderConfig } from './src/config'
