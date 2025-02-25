6ae04299272c570a6247a30b9384f7d62d223d86
joel <joelhooks@gmail.com>
2025-02-25 15:41:48 -0800

    refactor(posts): restructure posts module for better maintainability
    This completes Phase 1-3 of the refactoring plan: extracted types/schemas/utils to dedicated files, moved TypeSense operations to external dir, and extracted read operations to posts/read.ts. Reduced size of posts-query.ts and added JSDoc comments.

---
+++ apps/egghead/src/lib/posts/index.ts
@@ -0,0 +1,11 @@
+// Re-export types
+export * from './types'
+
+// Re-export schemas
+export * from './schemas'
+
+// Re-export utility functions
+export * from './utils'
+
+// Note: We're not re-exporting from '../posts' here to avoid circular references
+// This file is intended to replace it eventually
