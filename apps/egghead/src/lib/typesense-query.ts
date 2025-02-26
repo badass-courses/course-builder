c8c94e58e1a0359d8c69d6e49b05a1958eb347c2
joel <joelhooks@gmail.com>
2025-02-25 16:35:15 -0800

    refactor: flatten TypeSense module structure
    - Move TypeSense from lib/external/typesense to lib/typesense\n- Maintain backward compatibility through direct imports\n- Remove unnecessary directory nesting

--- apps/egghead/src/lib/typesense-query.ts
+++ apps/egghead/src/lib/typesense-query.ts
@@ -1,7 +1,7 @@
 /**
  * This file is preserved for backward compatibility.
- * All functionality has been moved to the `external/typesense` directory.
+ * All functionality has been moved to the `typesense` directory.
  */
 
 // Re-export all operations
-export { upsertPostToTypeSense } from './external/typesense'
+export { upsertPostToTypeSense } from './typesense/post'
