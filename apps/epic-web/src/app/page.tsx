8ba4926cea46b138c3a1180d51c8dd4b037f9ab9
Creeland <cree@provinsal.com>
2025-03-25 09:31:53 -0600

    feat(epic-web): refactor HomePage to PlaygroundPage with landing component
    - Replaced the HomePage component with PlaygroundPage, incorporating a new metadata structure.
    - Integrated the Landing component for improved content presentation.
    - Removed previous layout and links to streamline the page structure.

--- apps/epic-web/src/app/page.tsx
+++ apps/epic-web/src/app/page.tsx
@@ -1,33 +1,20 @@
-import Link from 'next/link'
+import * as React from 'react'
+import { type Metadata } from 'next'
+import { Landing } from '@/components/landing'
+import { coursebuilder } from '@/coursebuilder/course-builder-config'
 
-export default function HomePage() {
+export const metadata: Metadata = {
+	title: 'Epic React Builder',
+	description: 'Epic React Builder for creating content for Epic React',
+}
+
+export default async function PlaygroundPage() {
+	const cb = await coursebuilder()
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-center">
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-				<h1 className="text-5xl font-bold tracking-tight sm:text-[5rem]">
-					Epic Web Platform
-				</h1>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-						href="/posts"
-					>
-						<h3 className="text-2xl font-bold">Content →</h3>
-						<div className="text-lg">
-							Access Kent C. Dodds&apos; epic web content
-						</div>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-						href="/login"
-					>
-						<h3 className="text-2xl font-bold">Login →</h3>
-						<div className="text-lg">
-							Sign in to access your account and manage content
-						</div>
-					</Link>
-				</div>
-			</div>
-		</div>
+		<main>
+			<article className="prose sm:prose-lg dark:prose-invert mx-auto w-full max-w-2xl px-5 py-8 sm:py-16">
+				<Landing />
+			</article>
+		</main>
 	)
 }
