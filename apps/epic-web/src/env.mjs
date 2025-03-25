543dd10e3ef642f415ef3960f3e29d664540a3e3
Creeland <cree@provinsal.com>
2025-03-25 12:12:38 -0600

    chore: update environment variable handling in Turbo configuration and CI workflows
    - Added SKIP_ENV_VALIDATION environment variable to turbo.json, GitHub Actions workflows, and build scripts to streamline the build process.
    - Ensured consistent handling of SKIP_ENV_VALIDATION across various jobs in apps.yaml and ci.yaml for improved flexibility during development.

--- apps/epic-web/src/env.mjs
+++ apps/epic-web/src/env.mjs
@@ -35,7 +35,7 @@ export const env = createEnv({
 				: z.string().optional(),
 		NEXTAUTH_URL: z.preprocess(
 			// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-			// Since NextAuth.js automatically uses the VERCEL_URL if present.
+			// Since NextAuth.js automatic\ally uses the VERCEL_URL if present.
 			(str) =>
 				process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : str,
 			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
