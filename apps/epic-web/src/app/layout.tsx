f3c7c30b280121f4739dcc8a7b0016b32b390817
Creeland <cree@provinsal.com>
2025-03-25 10:17:21 -0600

    feat(epic-web): bring in trpc and stuff

--- apps/epic-web/src/app/layout.tsx
+++ apps/epic-web/src/app/layout.tsx
@@ -1,40 +1,26 @@
-import { cn } from '@/utils/cn'
-
 import '@/styles/globals.css'
 
-import { Providers } from './providers'
+import * as React from 'react'
+import { Inter } from 'next/font/google'
+import Navigation from '@/components/navigation'
+import { Party } from '@/components/party'
+import { Providers } from '@/components/providers'
+import { ThemeProvider } from '@/components/theme-provider'
+import { TRPCReactProvider } from '@/trpc/react'
+import { ourFileRouter } from '@/uploadthing/core'
+import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin'
+import { AxiomWebVitals } from 'next-axiom'
+import { extractRouterConfig } from 'uploadthing/server'
 
-const PUBLIC_TITLE = 'Epic Web'
+const inter = Inter({
+	subsets: ['latin'],
+	variable: '--font-sans',
+})
 
 export const metadata = {
-	title: {
-		default: PUBLIC_TITLE,
-		template: `%s | ${PUBLIC_TITLE}`,
-	},
-	description: 'The Epic Web Platform by Kent C. Dodds',
-	openGraph: {
-		title: PUBLIC_TITLE,
-		description: 'The Epic Web Platform by Kent C. Dodds',
-		url: process.env.NEXT_PUBLIC_URL,
-		siteName: PUBLIC_TITLE,
-		locale: 'en_US',
-		type: 'website',
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			'max-video-preview': -1,
-			'max-image-preview': 'large',
-			'max-snippet': -1,
-		},
-	},
-	twitter: {
-		title: PUBLIC_TITLE,
-		card: 'summary_large_image',
-	},
+	title: 'Epic React Builder',
+	description: 'build Epic React content',
+	icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
 export default function RootLayout({
@@ -43,10 +29,37 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={cn('bg-background min-h-screen antialiased')}>
-				<Providers>{children}</Providers>
-			</body>
-		</html>
+		<Providers>
+			<html lang="en" suppressHydrationWarning={true}>
+				<AxiomWebVitals />
+				<body className={`font-sans ${inter.variable}`}>
+					<TRPCReactProvider>
+						<Party />
+						<ThemeProvider
+							attribute="class"
+							defaultTheme="system"
+							enableSystem
+							disableTransitionOnChange
+						>
+							<div key="1" className="flex min-h-screen w-full flex-col">
+								<Navigation />
+								<main className="flex min-h-[calc(100vh-var(--nav-height))] flex-col">
+									<NextSSRPlugin
+										/**
+										 * The `extractRouterConfig` will extract **only** the route configs from the
+										 * router to prevent additional information from being leaked to the client. The
+										 * data passed to the client is the same as if you were to fetch
+										 * `/api/uploadthing` directly.
+										 */
+										routerConfig={extractRouterConfig(ourFileRouter)}
+									/>
+									{children}
+								</main>
+							</div>
+						</ThemeProvider>
+					</TRPCReactProvider>
+				</body>
+			</html>
+		</Providers>
 	)
 }
