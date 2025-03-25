b6e38eb3442819d86d98c0a9ec75f7905cf337e7
Creeland <cree@provinsal.com>
2025-03-25 16:52:46 -0600

    chore(epic-web): bring in utils and fix some types

--- apps/epic-web/src/app/api/chat/route.ts
+++ apps/epic-web/src/app/api/chat/route.ts
@@ -37,7 +37,7 @@ async function handler(req: Request) {
 
 	const { messages } = await req.json()
 	const response = await openai.chat.completions.create({
-		model: env.OPENAI_MODEL_ID,
+		model: env.OPENAI_MODEL_ID ?? 'gpt-3.5-turbo',
 		stream: true,
 		messages,
 	})
