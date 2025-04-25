134c80919623a55dd194e66975325541fdd9a632
Vojta Holik <vojta@egghead.io>
2025-04-25 16:09:49 +0200

    fix(cpro): update image URL for cursor pro card

--- apps/cursor-pro/src/config.ts
+++ apps/cursor-pro/src/config.ts
@@ -44,7 +44,7 @@ const config = {
 		},
 		images: [
 			{
-				url: `https://res.cloudinary.com/johnlindquist/image/upload/v1745580279/cursorpro-card_2x_dfcvx3.jpg`,
+				url: `https://res.cloudinary.com/johnlindquist/image/upload/v1745590169/cursorpro-card_2x_nplt99.jpg`,
 				width: 1200,
 				height: 630,
 			},
