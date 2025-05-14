702067b50e5323d59d038d6bd16cd77c2713d5af
Nicoll Guarnizo <nicoll.g4@gmail.com>
2025-05-14 12:34:35 -0500

    feat(craft-of-ui): homepage layout and styles

--- apps/craft-of-ui/src/app/page.tsx
+++ apps/craft-of-ui/src/app/page.tsx
@@ -83,112 +83,116 @@ const Home = async (props: Props) => {
 	}
 
 	return (
-		<LayoutClient
-			className="static"
-			highlightedResource={firstPageResource}
-			withContainer
-		>
-			<div className="bg-grid min-h-screen overflow-auto pb-16">
-				<div className="mx-auto max-w-3xl px-4 pb-16 pt-16">
-					<header className="text-gray-900 dark:text-gray-100">
-						<h1 className="mb-4 flex items-center text-sm font-extralight text-gray-800 dark:text-gray-200">
-							The Craft of UI — by&nbsp;
-							<Link
-								aria-label="Jhey Tompkins"
-								className="flex items-center gap-x-2 text-gray-600 outline-red-500 transition-colors hover:text-red-500 dark:text-gray-300 dark:hover:text-red-400"
-								href="https://twitter.com/intent/follow?screen_name=jh3yy"
-								target="_blank"
-							>
-								Jhey Tompkins
-								<Image
-									className="aspect-square w-7 rounded-full bg-gray-500"
-									src="/headshot.jpeg"
-									alt="Jhey Tompkins"
-									width={42}
-									height={42}
-								/>
-							</Link>
-						</h1>
-						<p className="mb-6 font-serif text-4xl leading-none sm:text-5xl md:text-6xl">
-							What if you could build{' '}
-							<span className="text-red-500">anything</span>?
-						</p>
-					</header>
-
-					<main
-						className={cn(
-							'prose prose-lg dark:prose-invert',
-							'[&>p:not(:has(+ul))]:mb-8 [&_p]:text-gray-600 [&_p]:dark:text-gray-300',
-							'[&_ul]:mb-8 [&_ul]:mt-2 [&_ul]:flex [&_ul]:list-disc [&_ul]:flex-col [&_ul]:gap-y-2 [&_ul]:pl-6',
-							'[&_h2]:font-serif [&_h2]:text-3xl [&_h2]:font-normal [&_h2]:leading-none',
-							'[&_h2]:mb-4 [&_h2]:mt-20',
-						)}
-					>
-						{firstPageResource && (
-							<Link
-								className="mx-auto mb-8 flex max-w-full items-center justify-center gap-1 rounded-full border border-violet-500/20 bg-violet-100 px-3 py-1 text-sm font-medium text-violet-700 shadow-md shadow-violet-600/10 dark:border-violet-500/20 dark:bg-violet-500/20 dark:text-violet-200 dark:shadow-none"
-								href={firstPageResource.path}
-								prefetch
-							>
-								<span className="truncate overflow-ellipsis">
-									New {firstPageResource.type}:{' '}
-									<span className="underline">{firstPageResource?.title}</span>
-								</span>
-							</Link>
-						)}
-
-						<article className="mx-auto w-full max-w-3xl pb-10">
-							{page?.fields?.body ? (
-								<>
-									<MDXRemote
-										source={page?.fields?.body}
-										components={{
-											CenteredTitle,
-											Instructor,
-											Spacer,
-											Section,
-											CheckList,
-											Testimonial,
-											CldImage: (props) => <CldImage {...props} />,
-											SubscribeForm,
-											ThemeImage: (props) => <ThemeImage {...props} />,
-											PrimaryNewsletterCta: (props) => (
-												<PrimaryNewsletterCta
-													resource={firstPageResource}
-													className={cn(
-														'not-prose pb-10 sm:pb-16',
-														props.className,
-													)}
-													trackProps={{
-														event: 'subscribed',
-														params: {
-															location: 'home',
-														},
-													}}
-													{...props}
-												/>
-											),
-											MuxPlayer: (props) => <MuxPlayer {...props} />,
-											Video: ({ resourceId }: { resourceId: string }) => (
-												<MDXVideo resourceId={resourceId} />
-											),
-										}}
+		<div className="home-page-grid">
+			<LayoutClient
+				className="static"
+				highlightedResource={firstPageResource}
+				withContainer
+			>
+				<div className="min-h-screen overflow-auto pb-16">
+					<div className="mx-auto max-w-3xl px-4 pb-16 pt-16">
+						<header className="text-gray-900 dark:text-gray-100">
+							<h1 className="mb-4 flex items-center text-sm font-extralight text-gray-800 dark:text-gray-200">
+								The Craft of UI — by&nbsp;
+								<Link
+									aria-label="Jhey Tompkins"
+									className="flex items-center gap-x-2 text-gray-600 outline-red-500 transition-colors hover:text-red-500 dark:text-gray-300 dark:hover:text-red-400"
+									href="https://twitter.com/intent/follow?screen_name=jh3yy"
+									target="_blank"
+								>
+									Jhey Tompkins
+									<Image
+										className="aspect-square w-7 rounded-full bg-gray-500"
+										src="/headshot.jpeg"
+										alt="Jhey Tompkins"
+										width={42}
+										height={42}
 									/>
-									<JheyProfile />
-									<PrimaryNewsletterCta
-										title="You want to build exceptional user interfaces, I want to empower you to do so."
-										byline="Join the waitlist to learn more and get course launch updates."
-										actionLabel="Subscribe"
-									/>
-								</>
-							) : (
-								<p>Page body not found</p>
+								</Link>
+							</h1>
+							<p className="mb-6 font-serif text-4xl leading-none sm:text-5xl md:text-6xl">
+								What if you could build{' '}
+								<span className="text-red-500">anything</span>?
+							</p>
+						</header>
+
+						<main
+							className={cn(
+								'prose prose-lg dark:prose-invert',
+								'[&>p:not(:has(+ul))]:mb-8 [&_p]:text-gray-600 [&_p]:dark:text-gray-300',
+								'[&_ul]:mb-8 [&_ul]:mt-2 [&_ul]:flex [&_ul]:list-disc [&_ul]:flex-col [&_ul]:gap-y-2 [&_ul]:pl-6',
+								'[&_h2]:font-serif [&_h2]:text-3xl [&_h2]:font-normal [&_h2]:leading-none',
+								'[&_h2]:mb-4 [&_h2]:mt-20',
 							)}
-						</article>
-					</main>
+						>
+							{firstPageResource && (
+								<Link
+									className="mx-auto mb-8 flex max-w-full items-center justify-center gap-1 rounded-full border border-violet-500/20 bg-violet-100 px-3 py-1 text-sm font-medium text-violet-700 shadow-md shadow-violet-600/10 dark:border-violet-500/20 dark:bg-violet-500/20 dark:text-violet-200 dark:shadow-none"
+									href={firstPageResource.path}
+									prefetch
+								>
+									<span className="truncate overflow-ellipsis">
+										New {firstPageResource.type}:{' '}
+										<span className="underline">
+											{firstPageResource?.title}
+										</span>
+									</span>
+								</Link>
+							)}
+
+							<article className="mx-auto w-full max-w-3xl pb-10">
+								{page?.fields?.body ? (
+									<>
+										<MDXRemote
+											source={page?.fields?.body}
+											components={{
+												CenteredTitle,
+												Instructor,
+												Spacer,
+												Section,
+												CheckList,
+												Testimonial,
+												CldImage: (props) => <CldImage {...props} />,
+												SubscribeForm,
+												ThemeImage: (props) => <ThemeImage {...props} />,
+												PrimaryNewsletterCta: (props) => (
+													<PrimaryNewsletterCta
+														resource={firstPageResource}
+														className={cn(
+															'not-prose pb-10 sm:pb-16',
+															props.className,
+														)}
+														trackProps={{
+															event: 'subscribed',
+															params: {
+																location: 'home',
+															},
+														}}
+														{...props}
+													/>
+												),
+												MuxPlayer: (props) => <MuxPlayer {...props} />,
+												Video: ({ resourceId }: { resourceId: string }) => (
+													<MDXVideo resourceId={resourceId} />
+												),
+											}}
+										/>
+										<JheyProfile />
+										<PrimaryNewsletterCta
+											title="You want to build exceptional user interfaces, I want to empower you to do so."
+											byline="Join the waitlist to learn more and get course launch updates."
+											actionLabel="Subscribe"
+										/>
+									</>
+								) : (
+									<p>Page body not found</p>
+								)}
+							</article>
+						</main>
+					</div>
 				</div>
-			</div>
-		</LayoutClient>
+			</LayoutClient>
+		</div>
 	)
 }
 
