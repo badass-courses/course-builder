b16b8d7b94ce884fd866d5b0416822a330a652cc
joel <joelhooks@gmail.com>
2025-05-22 07:25:04 -0700

    fix: look for query param

--- packages/core/src/lib/actions/claimed.ts
+++ packages/core/src/lib/actions/claimed.ts
@@ -10,11 +10,12 @@ export async function claimed(
 ): Promise<ResponseInternal> {
 	if (!options.adapter) throw new Error('Adapter not found')
 
-	const { bulkCouponId } = z
-		.object({
-			bulkCouponId: z.string(),
-		})
-		.parse(request.body)
+	const bulkCouponId = z
+		.string()
+		.optional()
+		.parse(request.query?.bulkCouponId || request.body?.bulkCouponId)
+
+	if (!bulkCouponId) throw new Error('Bulk coupon ID is required')
 
 	const purchases =
 		await options.adapter.getPurchasesForBulkCouponId(bulkCouponId)
