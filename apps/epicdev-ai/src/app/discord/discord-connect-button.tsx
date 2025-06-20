7db2270c98cbf9f1ed04fd47305649e9a57d5dc5
Creeland <cree@provinsal.com>
2025-06-20 14:47:45 -0500

    feat(eai): add Discord connection functionality with redirect support

---
+++ apps/epicdev-ai/src/app/discord/discord-connect-button.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import * as React from 'react'
+import { Icon } from '@/components/brand/icons'
+import { env } from '@/env.mjs'
+import { signIn } from 'next-auth/react'
+
+import { Button } from '@coursebuilder/ui'
+
+export function DiscordConnectButton({
+	discordProvider,
+	children,
+}: {
+	discordProvider: { id: string; name: string }
+	children?: React.ReactNode
+}) {
+	return (
+		<Button
+			data-button=""
+			variant="outline"
+			className="h-full bg-black text-white"
+			onClick={() =>
+				signIn(discordProvider.id, {
+					callbackUrl: `${env.NEXT_PUBLIC_URL}/discord/redirect`,
+					redirect: true,
+				})
+			}
+		>
+			<Icon
+				className="mr-2 flex items-center justify-center"
+				name="Discord"
+				size="20"
+			/>
+			{children || 'Connect to Discord'}
+		</Button>
+	)
+}
