c768459a5921cd641dde09233fb582a70875b956
Vojta Holik <vojta@egghead.io>
2025-07-10 10:54:26 +0200

    feat(eai): enhance AdminDashboard with subscriber, user, and purchase metrics
    - Implemented subscriber count retrieval from ConvertKit API.
    - Added total registered users and purchase counts to the dashboard.
    - Introduced new functions for fetching posts with completion counts.
    - Improved UI layout for displaying metrics and product details.

---
+++ apps/epicdev-ai/src/lib/kit-query.ts
@@ -0,0 +1,43 @@
+import { env } from '@/env.mjs'
+
+export async function getFormSubscribers() {
+	if (!process.env.CONVERTKIT_V4_API_KEY || !env.CONVERTKIT_SIGNUP_FORM) {
+		throw new Error('CONVERTKIT_API_SECRET is not set')
+	}
+	try {
+		const response = await fetch(
+			`https://api.convertkit.com/v4/forms/${env.CONVERTKIT_SIGNUP_FORM}/subscribers?include_total_count=include_total_count&per_page=1&status=active`,
+			{
+				headers: {
+					'X-Kit-Api-Key': process.env.CONVERTKIT_V4_API_KEY,
+				},
+			},
+		)
+
+		const data = await response.json()
+		console.log(data)
+
+		const sevenDaysAgo = new Date()
+		sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7)
+		const subscribersWithinLastWeek = await fetch(
+			`https://api.convertkit.com/v4/forms/${env.CONVERTKIT_SIGNUP_FORM}/subscribers?include_total_count=include_total_count&per_page=1&created_after=${sevenDaysAgo.toISOString()}`,
+			{
+				headers: {
+					'X-Kit-Api-Key': process.env.CONVERTKIT_V4_API_KEY,
+				},
+			},
+		)
+		const subscribersWithinLastWeekData = await subscribersWithinLastWeek.json()
+		console.log(subscribersWithinLastWeekData)
+
+		return {
+			...data,
+			total_count: data.pagination.total_count,
+			subscribers_added_last_week:
+				subscribersWithinLastWeekData.pagination.total_count,
+		}
+	} catch (error) {
+		console.error(error)
+		return []
+	}
+}
