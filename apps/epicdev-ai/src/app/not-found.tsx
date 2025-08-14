888eadad8ef35fb3f73289c3850f4c6413d305d2
Vojta Holik <vojta@egghead.io>
2025-08-14 16:02:59 +0200

    fix(all): correct className typo in NotFound component across multiple apps

--- apps/epicdev-ai/src/app/not-found.tsx
+++ apps/epicdev-ai/src/app/not-found.tsx
@@ -19,7 +19,7 @@ export default function NotFound() {
 				<p className="pb-8 pt-4">
 					The page you are looking for does not exist.
 				</p>
-				<div className="items-cener flex flex-wrap justify-center gap-3 gap-y-4">
+				<div className="flex flex-wrap items-center justify-center gap-3 gap-y-4">
 					<Button asChild>
 						<Link href="/posts">Start Learning</Link>
 					</Button>
