a5a85dd1b9a69c9335fd80317e284b9bd5e3a471
Vojta Holik <vojta@egghead.io>
2025-10-01 09:31:24 +0200

    fix(aih): mdx video layout shift

--- apps/ai-hero/src/components/content/mdx-video.tsx
+++ apps/ai-hero/src/components/content/mdx-video.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import { api } from '@/trpc/react'
-import MuxPlayer from '@mux/mux-player-react/lazy'
+import type { MuxPlayerProps } from '@mux/mux-player-react'
+import MuxPlayer from '@mux/mux-player-react'
+
+import { cn } from '@coursebuilder/ui/utils/cn'
 
 import Spinner from '../spinner'
 
@@ -9,10 +12,14 @@ export default function MDXVideo({
 	resourceId,
 	thumbnailTime = 0,
 	poster,
+	className,
+	props,
 }: {
 	resourceId: string
 	thumbnailTime?: number
 	poster?: string
+	className?: string
+	props?: MuxPlayerProps
 }) {
 	const { data, status } = api.videoResources.get.useQuery({
 		videoResourceId: resourceId,
@@ -20,7 +27,12 @@ export default function MDXVideo({
 
 	if (status === 'pending')
 		return (
-			<div className="mb-12 flex aspect-video h-full w-full max-w-4xl items-center justify-center rounded border">
+			<div
+				className={cn(
+					'mb-5 flex aspect-video w-full max-w-4xl items-center justify-center rounded-lg border',
+					className,
+				)}
+			>
 				<Spinner className="h-6 w-6" />
 			</div>
 		)
@@ -28,8 +40,15 @@ export default function MDXVideo({
 	if (!data?.muxPlaybackId) return null
 
 	return (
-		<div className="flex aspect-video w-full max-w-4xl items-center overflow-hidden rounded">
+		<div
+			className={cn(
+				'flex aspect-video w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg',
+				className,
+			)}
+		>
 			<MuxPlayer
+				streamType="on-demand"
+				className="aspect-video w-full"
 				playbackRates={[0.75, 1, 1.25, 1.5, 1.75, 2]}
 				maxResolution="2160p"
 				minResolution="540p"
@@ -37,6 +56,8 @@ export default function MDXVideo({
 				playbackId={data.muxPlaybackId}
 				thumbnailTime={thumbnailTime}
 				poster={poster}
+				playsInline
+				{...props}
 			/>
 		</div>
 	)
