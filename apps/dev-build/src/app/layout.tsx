353ea02cbbe19e490c6d982cef05873f1f2e00fd
Vojta Holik <vojta@egghead.io>
2025-11-14 18:08:40 +0100

    style(dev): update HolyLoader color property to use CSS variable for consistency

--- apps/dev-build/src/app/layout.tsx
+++ apps/dev-build/src/app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
 						>
 							<NuqsAdapter>
 								<HolyLoader
-									color="hsl(var(--primary))"
+									color="var(--primary)"
 									height="0.15rem"
 									speed={250}
 								/>
