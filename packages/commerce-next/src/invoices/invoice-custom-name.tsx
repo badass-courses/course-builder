1a0ec39d608885d05e1967a5bf15e5fbe85927e4
Vojta Holik <vojta@egghead.io>
2025-11-24 13:11:26 +0100

    feat(aih): add ability to modify invoice name with new InvoiceCustomName component

---
+++ packages/commerce-next/src/invoices/invoice-custom-name.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import * as React from 'react'
+import { useLocalStorage } from 'react-use'
+
+import { Input, Textarea } from '@coursebuilder/ui'
+
+export function InvoiceCustomName({
+	defaultName,
+	chargeId,
+}: {
+	defaultName: string
+	chargeId: string
+}) {
+	const [invoiceName, setInvoiceName] = useLocalStorage(
+		`invoice-name`,
+		defaultName,
+	)
+	return (
+		<>
+			<Textarea
+				className="my-2 h-full w-full p-3 print:my-0 print:hidden print:border-none print:bg-transparent print:p-0 print:text-base"
+				aria-label="Invoice recipient name"
+				value={invoiceName}
+				onChange={(e) => setInvoiceName(e.target.value)}
+				placeholder="Enter name"
+			/>
+			<div className="hidden print:block">{invoiceName}</div>
+		</>
+	)
+}
